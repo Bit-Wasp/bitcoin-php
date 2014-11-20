59edf6c1981e21b597ab3776e38314dcf87e45b6
BitWasp <thomas@bitwasp.co>
2014-11-20 14:00:49 +0000

    Comments to math file

--- src/Math.php
+++ src/Math.php
@@ -10,13 +10,24 @@ namespace Bitcoin;
 
 class Math {
 
-    private static $adapter = null;
+    /**
+     * @var \Mdanter\Ecc\MathAdater
+     */
+    private static $adapter;
 
+    /**
+     * @param \Mdanter\Ecc\MathAdapter $math
+     */
     public static function setAdapter(\Mdanter\Ecc\MathAdapter $math)
     {
         self::$adapter = $math;
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
     public static function __callStatic($name, $arguments)
     {
         if (is_null(static::$adapter)) {
@@ -26,9 +37,19 @@ class Math {
         return call_user_func_array(array(static::$adapter, $name), $arguments);
     }
 
+    /**
+     * Similar to gmp_div_qr, return a tuple containing the
+     * result and the remainder
+     *
+     * @param $dividend
+     * @param $divisor
+     * @return array
+     */
     public static function div_qr($dividend, $divisor)
     {
+        // $div = n / q
         $div = Math::div($dividend, $divisor);
+        // $remainder = n - (n / q) * q
         $remainder = Math::sub($dividend, Math::mul($div, $divisor));
         return array($div, $remainder);
     }
