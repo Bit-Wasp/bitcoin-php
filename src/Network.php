dcc4233a9bfd59fa04c56b36c1c90a7dfb89c3e0
BitWasp <thomas@bitwasp.co>
2014-11-20 14:02:40 +0000

    Add comments to Network class

--- src/Network.php
+++ src/Network.php
@@ -11,23 +11,46 @@ namespace Bitcoin;
 
 class Network implements NetworkInterface {
 
+    /**
+     * @var
+     */
     protected $address_byte;
+
+    /**
+     * @var
+     */
     protected $priv_byte;
+
+    /**
+     * @var
+     */
     protected $p2sh_byte;
+
+    /**
+     * @var bool
+     */
     protected $testnet;
+
+    /**
+     * @var null
+     */
     protected $xpub_byte = null;
+
+    /**
+     * @var null
+     */
     protected $xpriv_byte = null;
 
     /**
+     * Load basic data, throw exception if it's not provided
      * @param $address_byte
      * @param $p2sh_byte
      * @param $priv_byte
-     * @throws \InvalidArgumentException
+     * @throws \Exception
      */
     public function __construct($address_byte, $p2sh_byte, $priv_byte, $testnet = FALSE)
     {
-        foreach(array('address_byte', 'p2sh_byte', 'priv_byte') as $required_byte)
-        {
+        foreach (array('address_byte', 'p2sh_byte', 'priv_byte') as $required_byte) {
             if (ctype_xdigit($$required_byte) and strlen($$required_byte) == 2) {
                 $this->$required_byte = $$required_byte;
             } else {
@@ -42,21 +65,33 @@ class Network implements NetworkInterface {
         $this->testnet = $testnet;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function isTestnet()
     {
         return $this->testnet;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getAddressByte()
     {
         return $this->address_byte;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getPrivByte()
     {
         return $this->priv_byte;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getP2shByte()
     {
         return $this->p2sh_byte;
