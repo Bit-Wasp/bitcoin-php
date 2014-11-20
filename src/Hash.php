2fa7654ac394ae103378989a0b9f9639d1f40c5d
BitWasp <thomas@bitwasp.co>
2014-11-20 13:57:31 +0000

    Comments for Hash class

--- src/Hash.php
+++ src/Hash.php
@@ -12,6 +12,13 @@ namespace Bitcoin;
 class Hash
 {
 
+    /**
+     * Calculate Sha256(RipeMd160()) on the given data
+     *
+     * @param $data
+     * @param bool $binary_output
+     * @return string
+     */
     public static function sha256ripe160($data, $binary_output = false)
     {
         $hash = self::sha256($data, true);
@@ -19,12 +26,26 @@ class Hash
         return $hash;
     }
 
+    /**
+     * Perform SHA256
+     *
+     * @param $data
+     * @param bool $binary_output
+     * @return string
+     */
     public static function sha256($data, $binary_output = false)
     {
         $hash = hash('sha256', $data, $binary_output);
         return $hash;
     }
 
+    /**
+     * Perform SHA256 twice
+     *
+     * @param $data
+     * @param bool $binary_output
+     * @return string
+     */
     public static function sha256d($data, $binary_output = false)
     {
         $hash = self::sha256($data, true);
@@ -32,18 +53,43 @@ class Hash
         return $hash;
     }
 
+    /**
+     * RIPEMD160
+     *
+     * @param $data
+     * @param bool $binary_output
+     * @return string
+     */
     public static function ripe160($data, $binary_output = false)
     {
         $hash = hash('ripemd160', $data, $binary_output);
         return $hash;
     }
 
+    /**
+     * RIPEMD160 twice
+     *
+     * @param $data
+     * @param bool $binary_output
+     * @return string
+     */
     public static function ripe160d($data, $binary_output = false) {
         $hash = self::ripe160($data, true);
         $hash = self::ripe160($hash, $binary_output);
         return $hash;
     }
 
+    /**
+     * PBKDF2
+     * 
+     * @param $algo
+     * @param $data
+     * @param $salt
+     * @param $iterations
+     * @param $length
+     * @param bool $binary_output
+     * @return mixed
+     */
     public static function pbkdf2($algo, $data, $salt, $iterations, $length, $binary_output = false)
     {
         return hash_pbkdf2($algo, $data, $salt, $iterations, $length, $binary_output);
