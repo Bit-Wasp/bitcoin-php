95b52e3ed65994b42bd516f9921000c8468ffde3
BitWasp <thomas@bitwasp.co>
2015-01-06 11:40:27 +0000

    Add a class for mining genesis blocks.

---
+++ src/Block/GenesisMiningBlockHeader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bitcoin\Block;
+
+/**
+ * Class GenesisMiningBlockHeader
+ * @package Bitcoin\Block
+ */
+class GenesisMiningBlockHeader extends BlockHeader
+{
+    public function getBlockHash()
+    {
+        return '0000000000000000000000000000000000000000000000000000000000000000';
+    }
+}
