2f35c10f83a485767e5b070778f46c2729e19b3d
BitWasp <thomas@bitwasp.co>
2015-01-19 05:29:56 +0000

    Remove sign tests from PrivateKeyTest

--- tests/Key/PrivateKeyTest.php
+++ tests/Key/PrivateKeyTest.php
@@ -206,34 +206,5 @@ class PrivateKeyTest extends \PHPUnit_Framework_TestCase
         PrivateKey::fromWif('50akdglashdgkjadsl');
     }
 
-    public function testSign()
-    {
-
-        $f = file_get_contents(__DIR__.'/../Data/hmacdrbg.json');
-
-        $json = json_decode($f);
-        $math = Bitcoin::getMath();
-        $generator = Bitcoin::getGenerator();
-        $signer = new \Bitcoin\Signature\Signer($math, $generator);
-
-        foreach ($json->test as $c => $test) {
-
-            $privateKey = new PrivateKey($test->privKey);
-            $message = new Buffer($test->message);
-            $messageHash = new Buffer(Hash::sha256($message->serialize(), true));
-
-            $k = new \Bitcoin\Signature\K\DeterministicK($privateKey, $messageHash);
-            $sig = $signer->sign($privateKey, $messageHash, $k);
-
-            // K must be correct (from privatekey and message hash)
-            $this->assertEquals(Buffer::hex($test->expectedK), $k->getK());
-
-            // R and S should be correct
-            $rHex = $math->dechex($sig->getR());
-            $sHex = $math->decHex($sig->getS());
-            $this->assertSame($test->expectedRSLow, $rHex.$sHex);
-
-        }
-    }
 
 }
