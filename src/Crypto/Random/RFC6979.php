555fd36f70f822648e7667cb63bf67f0a10fb05a
BitWasp <thomas@bitwasp.co>
2015-02-11 05:38:22 +0000

    Update RFC6979 class

--- src/Crypto/Random/RFC6979.php
+++ src/Crypto/Random/RFC6979.php
@@ -2,74 +2,77 @@
 
 namespace Bitcoin\Crypto\Random;
 
-use Bitcoin\Bitcoin;
 use Bitcoin\Buffer;
-use Bitcoin\Crypto\Hash;
 use Bitcoin\Key\PrivateKeyInterface;
 use Bitcoin\Math\Math;
 use Mdanter\Ecc\GeneratorPoint;
 
 /**
- * Class RFC6979 - This class is used to derive a deterministic nonce from
- * a private key and a message.
- *
- * @package Bitcoin\Crypto\DRBG
+ * Class DeterministicK
+ * @package Bitcoin\Signature\K
  * @author Thomas Kerin
+ * Todo: refactor so this class accepts an initialized DRBGInterface
  */
 class RFC6979 implements RBGInterface
 {
+
     /**
      * @var HMACDRBG
      */
-    private $drbg;
-
-    /**
-     * @var GeneratorPoint
-     */
-    private $generator;
+    protected $drbg;
 
     /**
      * @var Math
      */
-    private $math;
+    protected $math;
 
     /**
-     * @param $algo
-     * @param PrivateKeyInterface $privateKey
-     * @param \Bitcoin\Buffer $message
+     * @var GeneratorPoint
      */
-    public function __construct(Math $math, $generator, $privateKey, Buffer $message, $algo = 'sha256')
+    protected $generator;
+
+    /**
+     * @var Buffer
+     */
+    protected $k;
+
+    /**
+     * @param PrivateKeyInterface $privateKey
+     * @param Buffer $messageHash
+     * @param string $algo
+     */
+    public function __construct(Math $math, GeneratorPoint $generator, PrivateKeyInterface $privateKey, Buffer $messageHash, $algo = 'sha256')
     {
-        $this->generator = $generator;
         $this->math      = $math;
-        $entropy         = new Buffer($privateKey->serialize() . Hash::sha256($message, true));
-        $this->generator = Bitcoin::getGenerator();
+        $this->generator = $generator;
+        $entropy         = new Buffer($privateKey->serialize() . $messageHash->serialize());
         $this->drbg      = new HMACDRBG($algo, $entropy);
+        return $this;
     }
 
     /**
-     * Return a sequence of $numBytes bytes, which are between [1..Q]
-     * @param int $numBytes
-     * @return \Bitcoin\Buffer
+     * Return a K value deterministically derived from the private key
+     *  and data
+     *
+     * @return Buffer
      */
     public function bytes($numBytes)
     {
-        $math = Bitcoin::getMath();
+        if (is_null($this->k)) {
+            while (true) {
+                $this->k    = $this->drbg->bytes($numBytes);
+                $kInt = $this->k->serialize('int');
 
-        while (true) {
-            $k    = $this->drbg->bytes($numBytes);
-            $kInt = $k->serialize('int');
+                // Check k is between [1, ... Q]
+                if ($this->math->cmp(1, $kInt) <= 0 && $this->math->cmp($kInt, $this->generator->getOrder()) < 0) {
+                    break;
+                }
 
-            // Check k is between [1, ... Q]
-            if ($math->cmp(1, $kInt) <= 0
-                and $math->cmp($kInt, $this->generator->getOrder()) < 0) {
-                break;
+                // Otherwise derive another and try again.
+                $this->drbg->update(null);
             }
-
-            // Otherwise derive another and try again.
-            $this->drbg->update(null);
         }
 
-        return $k;
+        return $this->k;
     }
 }
