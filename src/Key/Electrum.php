ba5f4ba1707513d66846b15eb5bcddfe274be0c2
Thomas Kerin <thomas@bitwasp.co>
2015-04-24 00:13:29 +0100

    Add start of Electrum key derivation

---
+++ src/Key/Electrum.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BitWasp\Bitcoin\Key;
+
+use BitWasp\Bitcoin\Crypto\Hash;
+use BitWasp\Bitcoin\Key\PrivateKeyInterface;
+use BitWasp\Bitcoin\Crypto\EcAdapter\EcAdapterInterface;
+use BitWasp\Buffertools\Buffer;
+
+class Electrum
+{
+    private $ecAdapter;
+
+    /**
+     * @var PrivateKeyInterface|PublicKeyInterface
+     */
+    private $masterKey;
+
+    /**
+     * @param EcAdapterInterface $ecAdapter
+     * @param KeyInterface|PrivateKeyInterface|PublicKeyInterface $masterKey
+     */
+    public function __construct(EcAdapterInterface $ecAdapter, KeyInterface $masterKey)
+    {
+        $this->ecAdapter = $ecAdapter;
+        $this->masterKey = $masterKey;
+    }
+
+    /**
+     * @return PublicKeyInterface
+     */
+    public function getMasterPublicKey()
+    {
+        $key = $this->masterKey;
+
+        return $key instanceof PrivateKeyInterface
+            ? $this->masterKey->getPublicKey()
+            : $this->masterKey;
+    }
+
+    /**
+     * @param $sequence
+     * @param bool $change
+     * @return int|string
+     */
+    public function getSequenceOffset($sequence, $change = false)
+    {
+        $offsetBuf = new Buffer("$sequence:"
+            . ($change ? '1' : '0')
+            . $this->getMasterPublicKey()->getBinary());
+
+        return Hash::sha256d($offsetBuf)->getInt();
+    }
+
+    /**
+     * @param $sequence
+     * @param bool $change
+     */
+    public function deriveChild($sequence, $change = false)
+    {
+        $key = $this->masterKey;
+
+        
+    }
+}
