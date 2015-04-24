1ce52291a0cf5f2a5a7148ac558a80606e7d7251
Thomas Kerin <thomas@bitwasp.co>
2015-04-24 13:10:14 +0100

    Finish ElectrumMnemonic entropyToMnemonic/entropyToWords

--- src/Mnemonic/Electrum/ElectrumMnemonic.php
+++ src/Mnemonic/Electrum/ElectrumMnemonic.php
@@ -2,7 +2,6 @@
 
 namespace BitWasp\Bitcoin\Mnemonic\Electrum;
 
-
 use BitWasp\Bitcoin\Crypto\EcAdapter\EcAdapterInterface;
 use BitWasp\Bitcoin\Mnemonic\MnemonicInterface;
 use BitWasp\Buffertools\Buffer;
@@ -34,18 +33,16 @@ class ElectrumMnemonic implements MnemonicInterface
         $n = count($this->wordList);
         $wordArray = [];
 
-        $chunks = $entropy->getSize() / 8;
+        $chunks = $entropy->getSize() / 4;
         for ($i = 0; $i < $chunks; $i++) {
-            $x = $entropy->slice(8*$i, 8)->getInt();
+            $x = $entropy->slice(4*$i, 4)->getInt();
             $index1 = $math->mod($x, $n);
-            $index2 = $math->add($math->div($x, $n), $index1);
-            $index3 = $math->add($math->div($math->div($x, $n), $n), $index1);
+            $index2 = $math->mod($math->add($math->div($x, $n), $index1), $n);
+            $index3 = $math->mod($math->add($math->div($math->div($x, $n), $n), $index2), $n);
 
-            $wordArray += [
-                $this->wordList->getWord($index1),
-                $this->wordList->getWord($index2),
-                $this->wordList->getWord($index3)
-            ];
+            array_push($wordArray, $this->wordList->getWord($index1));
+            array_push($wordArray, $this->wordList->getWord($index2));
+            array_push($wordArray, $this->wordList->getWord($index3));
         }
 
         return $wordArray;
@@ -78,12 +75,12 @@ class ElectrumMnemonic implements MnemonicInterface
         for ($i = 0; $i < $thirdWordCount; $i++) {
             $a = $math->mul(3, $i);
             list ($word1, $word2, $word3) = array_slice($words, $a, 3);
-
             $index1 = $wordList->getIndex($word1);
             $index2 = $wordList->getIndex($word2);
             $index3 = $wordList->getIndex($word3);
 
-            $x = $math->add($index1,
+            $x = $math->add(
+                $index1,
                 $math->add(
                     $math->mul(
                         $n,
@@ -98,7 +95,6 @@ class ElectrumMnemonic implements MnemonicInterface
                     )
                 )
             );
-
             $out .= $math->decHex($x);
         }
 
