13e0087de2840f60007a05b392eccdd2a45d605f
Thomas Kerin <thomas@bitwasp.co>
2015-04-25 12:35:37 +0100

    Should directly instantiate math/generator/phpecc/secp256k1 classes also should let people run faster (secp256k1 only) tests by setting anything to EXT_SECP256K1

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -2,9 +2,9 @@
 
 namespace BitWasp\Bitcoin\Tests;
 
-use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Crypto\EcAdapter\PhpEcc;
 use BitWasp\Bitcoin\Crypto\EcAdapter\Secp256k1;
+use Mdanter\Ecc\EccFactory;
 
 abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
 {
@@ -14,20 +14,30 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
      */
     public function getEcAdapters()
     {
-        $math = Bitcoin::getMath();
-        $generator = Bitcoin::getGenerator();
+        $math = new \BitWasp\Bitcoin\Math\Math;
+        $generator = EccFactory::getSecgCurves()->generator256k1();
         $adapters = [];
 
         if (getenv('TRAVIS_PHP_VERSION')) {
+            // If travis
+            // If EXT_SECP256K1 env var is set, only return secp256k1.
+            // Otherwise return phpecc
             if (strlen(getenv('EXT_SECP256K1')) == 0) {
                 $adapters[] = [new PhpEcc($math, $generator)];
             } else {
                 $adapters[] = [new Secp256k1($math, $generator)];
             }
         } else {
-            $adapters[] = [new PhpEcc($math, $generator)];
+            // If NOT travis
+            // Return both adapters if EXT_SECP256K1 is not set (secp256k1 only returned if ext is found)
+            if (strlen(getenv('EXT_SECP256K1')) == 0) {
+                $adapters[] = [new PhpEcc($math, $generator)];
 
-            if (extension_loaded('secp256k1')) {
+                if (extension_loaded('secp256k1')) {
+                    $adapters[] = [new Secp256k1($math, $generator)];
+                }
+            } else {
+                // Env var was set, just pass secp256k1
                 $adapters[] = [new Secp256k1($math, $generator)];
             }
         }
