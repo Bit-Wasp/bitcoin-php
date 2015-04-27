c2091d8b9cdd7ba4aa67b5bbcdb93e7fe600fdb2
Thomas Kerin <thomas@bitwasp.co>
2015-04-27 15:28:20 +0100

    Add Japanese word list and test

---
+++ src/Mnemonic/Bip39/Wordlist/JapaneseWordList.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 27/04/15
+ * Time: 15:10
+ */
---
+++ tests/Mnemonic/Bip39/WordList/JapaneseWordListTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 27/04/15
+ * Time: 15:17
+ */
+
+namespace Mnemonic\Bip39\WordList;
+
+
+class JapaneseWordListTest {
+
+}
