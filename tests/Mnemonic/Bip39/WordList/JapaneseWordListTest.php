c7cd2eb3d3e8dad454e1efff3b61fe6b67d34042
Thomas Kerin <thomas@bitwasp.co>
2015-04-27 15:28:39 +0100

    JP wordlist/test

--- tests/Mnemonic/Bip39/WordList/JapaneseWordListTest.php
+++ tests/Mnemonic/Bip39/WordList/JapaneseWordListTest.php
@@ -1,14 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thomas
- * Date: 27/04/15
- * Time: 15:17
- */
 
-namespace Mnemonic\Bip39\WordList;
+namespace BitWasp\Bitcoin\Tests\Mnemonic\Bip39\WordList;
 
+use BitWasp\Bitcoin\Mnemonic\Bip39\Wordlist\EnglishWordList;
+use BitWasp\Bitcoin\Tests\AbstractTestCase;
 
-class JapaneseWordListTest {
+class JapaneseWordListTest extends AbstractTestCase
+{
+    public function testGetWordList()
+    {
+        $wl = new \BitWasp\Bitcoin\Mnemonic\Bip39\Wordlist\JapaneseWordList();
+        $this->assertEquals(2048, count($wl));
+        $this->assertEquals(2048, count($wl->getWords()));
+    }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testUnknownWord()
+    {
+        $wl = new \BitWasp\Bitcoin\Mnemonic\Bip39\Wordlist\JapaneseWordList();
+        $wl->getWord(101010101);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testExceptionOutOfRange()
+    {
+        $wl = new \BitWasp\Bitcoin\Mnemonic\Bip39\Wordlist\JapaneseWordList();
+
+        $word = $wl->getIndex('あいだ');
+        $this->assertInternalType('integer', $word);
+
+        $wl->getIndex('あいあいあい');
+    }
 }
