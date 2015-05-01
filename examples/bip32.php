f7c7093db350959324eb240d79960b226b241437
Thomas Kerin <thomas@bitwasp.co>
2015-05-01 15:38:27 +0100

    Simplify bip32 example

--- examples/bip32.php
+++ examples/bip32.php
@@ -10,13 +10,21 @@ $math = Bitcoin::getMath();
 $network = Bitcoin::getNetwork();
 
 $master = HierarchicalKeyFactory::generateMasterKey();
-echo $master->toExtendedPrivateKey($network) . "\n";
-echo "Address: " . $master->getPublicKey()->getAddress() . "\n\n";
+echo "Master key (m)\n";
+echo "   " . $master->toExtendedPrivateKey($network) . "\n";
+echo "   Address: " . $master->getPublicKey()->getAddress()->getAddress() . "\n\n";
+echo "Derive sequential keys:\n";
 
 $key1 = $master->deriveChild($master->getHardenedSequence(0));
-echo $key1->toExtendedPrivateKey($network) . "\n";
-echo "Address: " . $key1->getPublicKey()->getAddress() . "\n\n";
+echo " - m/0' " . $key1->toExtendedPrivateKey($network) . "\n";
+echo "   Address: " . $key1->getPublicKey()->getAddress()->getAddress() . "\n\n";
 
 $key2 = $key1->deriveChild(999999);
-echo $key2->toExtendedPublicKey($network) . "\n";
-echo "Address: " . $key2->getPublicKey()->getAddress() . "\n\n";
+echo " - m/0'/999999 " . $key2->toExtendedPublicKey($network) . "\n";
+echo "   Address: " . $key2->getPublicKey()->getAddress()->getAddress() . "\n\n";
+
+echo "Directly derive path\n";
+
+$sameKey2 = $master->derivePath("0'/999999");
+echo " - m/0'/999999 " . $sameKey2->toExtendedPublicKey() . "\n";
+echo "   Address: " . $sameKey2->getPublicKey()->getAddress()->getAddress() . "\n\n";
