d7dbd628e6b06cff58451bac2a70e978739e5b94
Thomas Kerin <thomas@bitwasp.co>
2015-05-01 14:08:18 +0100

    Commit step-by-step tests for building each transaction type with TransactionBuilder

--- tests/Transaction/TransactionBuilderTest.php
+++ tests/Transaction/TransactionBuilderTest.php
@@ -14,6 +14,8 @@ use BitWasp\Bitcoin\Script\ScriptInterface;
 use BitWasp\Bitcoin\Tests\AbstractTestCase;
 use BitWasp\Bitcoin\Transaction\Transaction;
 use BitWasp\Bitcoin\Transaction\TransactionBuilder;
+use BitWasp\Bitcoin\Script\Classifier\InputClassifier;
+use BitWasp\Bitcoin\Transaction\TransactionBuilderInputState;
 use BitWasp\Bitcoin\Transaction\TransactionFactory;
 use BitWasp\Bitcoin\Transaction\TransactionInput;
 use BitWasp\Bitcoin\Transaction\TransactionOutput;
@@ -21,6 +23,9 @@ use BitWasp\Bitcoin\Key\PrivateKeyInterface;
 
 class TransactionBuilderTest extends AbstractTestCase
 {
+    public $txBldrType = 'BitWasp\Bitcoin\Transaction\TransactionBuilder';
+    public $txBldrStateType = 'BitWasp\Bitcoin\Transaction\TransactionBuilderInputState';
+
     public function testDefaultTransaction()
     {
         $tx = new Transaction();
@@ -331,5 +336,192 @@ class TransactionBuilderTest extends AbstractTestCase
         $this->assertTrue($builder->getInputState(0)->isFullySigned());
 
         $this->assertEquals('0100000001aafb9e229a0d5b18039724aa65c31eef2a1079210d38dc94b18e66cf84def84600000000fd1b0100483045022100a7fa1c1e7e37808175a2a17c913498623fb22c74a62605ee98c7c69d64425d3902204324efb27e5374b4b3e0637f58cac66b9e0349e18adfa176d20d50e3932a52910147304402205a490d36c2f26cbed936b2b35984eb6906e01918bee30e15a017703120360c3802201d3e0c02afccff356113e50f831a63b5bef1d913aee10731cf072b85c40cc12e014c8752410443f3ce7c4ddf438900a6662420511ea48321f8cedd3e63943700b07ac9752a6bf18230095730b18f2d3c3dbdc0a892ca62b1722730f183d370963d6f4d3e20c84104f260c8b554e9d0921c507fb231d0e226ba17462078825c56170facb6567dcec700750bd529f4361da21f59fbfc7d0bce319fdef4e7c524e82d3e313e92b1b34752aeffffffff0000000000', $builder->getTransaction()->getHex());
+    }
+
+    public function testIncrementallySigningP2PK()
+    {
+        $ecAdapter = Bitcoin::getEcAdapter();
+
+        $pk1 = PrivateKeyFactory::fromHex('421c76d77563afa1914846b010bd164f395bd34c2102e5e99e0cb9cf173c1d87');
+        $outputScript = ScriptFactory::scriptPubKey()->payToPubKey($pk1->getPublicKey());
+
+        // This is the transaction we are pretending exists in the blockchain
+        $spendTx = new Transaction();
+        $spendTx->getInputs(0)->addInput(new TransactionInput(
+            '4141414141414141414141414141414141414141414141414141414141414141',
+            0
+        ));
+        $spendTx->getOutputs(0)->addOutput(new TransactionOutput(
+            50,
+            $outputScript
+        ));
+
+        // Now we build a transaction spending it
+        $builder = new TransactionBuilder($ecAdapter);
+        $builder->spendOutput($spendTx, 0);
+        $builder->payToAddress($pk1->getAddress(), 50);
+        try {
+            $builder->getInputState(0);
+        } catch (BuilderNoInputState $e) {
+            $this->assertTrue(!!$e);
+        }
+
+        // Take the built transaction, pass it to a new TransactionBuilder class, and manually create input state
+        $builder = new TransactionBuilder($ecAdapter, TransactionFactory::fromHex($builder->getTransaction()->getHex()));
+        $builder->createInputState(0, $outputScript);
+
+        // Check InputState was correctly set up
+        $is = $builder->getInputState(0);
+        $this->assertEquals(InputClassifier::PAYTOPUBKEY, $is->getScriptType());
+        $this->assertInstanceOf($this->txBldrStateType, $is);
+        $this->assertEquals(0, $is->getSigCount());
+        $this->assertEquals(1, $is->getRequiredSigCount());
+
+        // Check input is correctly set up
+        $i = $builder->getTransaction()->getInputs()->getInput(0);
+        $this->assertEquals($spendTx->getTransactionId(), $i->getTransactionId());
+        $this->assertEquals(0, $i->getVout());
+        $this->assertEquals(new Script(), $i->getScript());
+
+        // Check output is correctly set up
+        $o = $builder->getTransaction()->getOutputs()->getOutput(0);
+        $this->assertEquals(50, $o->getValue());
+        $this->assertEquals(ScriptFactory::scriptPubKey()->payToPubKeyHash($pk1->getPublicKey()), $o->getScript());
+
+        // Do signing
+        $builder->signInputWithKey($pk1, $outputScript, 0);
+
+        // Check that all signatures and state were preserved
+        $builder = new TransactionBuilder($ecAdapter, $builder->getTransaction());
+        $builder->createInputState(0, $outputScript);
+        $this->assertEquals(1, $builder->getInputState(0)->getSigCount());
+        $this->assertTrue($builder->getInputState(0)->isFullySigned());
+    }
+
+    public function testIncrementallySigningP2PKH()
+    {
+        $ecAdapter = Bitcoin::getEcAdapter();
+
+        $pk1 = PrivateKeyFactory::fromHex('421c76d77563afa1914846b010bd164f395bd34c2102e5e99e0cb9cf173c1d87');
+        $outputScript = ScriptFactory::scriptPubKey()->payToPubKeyHash($pk1->getPublicKey());
+
+        // This is the transaction we are pretending exists in the blockchain
+        $spendTx = new Transaction();
+        $spendTx->getInputs(0)->addInput(new TransactionInput(
+            '4141414141414141414141414141414141414141414141414141414141414141',
+            0
+        ));
+        $spendTx->getOutputs(0)->addOutput(new TransactionOutput(
+            50,
+            $outputScript
+        ));
+
+        // Now we build a transaction spending it
+        $builder = new TransactionBuilder($ecAdapter);
+        $builder->spendOutput($spendTx, 0);
+        $builder->payToAddress($pk1->getAddress(), 50);
+        try {
+            $builder->getInputState(0);
+        } catch (BuilderNoInputState $e) {
+            $this->assertTrue(!!$e);
+        }
+
+        // Take the built transaction, pass it to a new TransactionBuilder class, and manually create input state
+        $builder = new TransactionBuilder($ecAdapter, TransactionFactory::fromHex($builder->getTransaction()->getHex()));
+        $builder->createInputState(0, $outputScript);
+
+        // Check InputState was correctly set up
+        $is = $builder->getInputState(0);
+
+        $this->assertInstanceOf($this->txBldrStateType, $is);
+        $this->assertEquals(InputClassifier::PAYTOPUBKEYHASH, $is->getScriptType());
+        $this->assertEquals(0, $is->getSigCount());
+        $this->assertEquals(1, $is->getRequiredSigCount());
+
+        // Check input is correctly set up
+        $i = $builder->getTransaction()->getInputs()->getInput(0);
+        $this->assertEquals($spendTx->getTransactionId(), $i->getTransactionId());
+        $this->assertEquals(0, $i->getVout());
+        $this->assertEquals(new Script(), $i->getScript());
+
+        // Check output is correctly set up
+        $o = $builder->getTransaction()->getOutputs()->getOutput(0);
+        $this->assertEquals(50, $o->getValue());
+        $this->assertEquals(ScriptFactory::scriptPubKey()->payToPubKeyHash($pk1->getPublicKey()), $o->getScript());
+
+        // Do signing
+        $builder->signInputWithKey($pk1, $outputScript, 0);
+
+        // Check that all signatures and state were preserved
+        $builder = new TransactionBuilder($ecAdapter, $builder->getTransaction());
+        $builder->createInputState(0, $outputScript);
+        $this->assertEquals(1, $builder->getInputState(0)->getSigCount());
+        $this->assertTrue($builder->getInputState(0)->isFullySigned());
+    }
+
+    public function testIncrementallySigningP2SHMultisig()
+    {
+        $ecAdapter = Bitcoin::getEcAdapter();
+
+        $pk1 = PrivateKeyFactory::fromHex('421c76d77563afa1914846b010bd164f395bd34c2102e5e99e0cb9cf173c1d87');
+        $pk2 = PrivateKeyFactory::fromHex('f7225388c1d69d57e6251c9fda50cbbf9e05131e5adb81e5aa0422402f048162');
+
+        $redeemScript = ScriptFactory::multisig(2, [$pk1->getPublicKey(), $pk2->getPublicKey()]);
+        $outputScript = $redeemScript->getOutputScript();
+
+        $spendTx = new Transaction();
+        $spendTx->getInputs(0)->addInput(new TransactionInput(
+            '4141414141414141414141414141414141414141414141414141414141414141',
+            0
+        ));
+        $spendTx->getOutputs(0)->addOutput(new TransactionOutput(
+            50,
+            $outputScript
+        ));
+
+        $builder = new TransactionBuilder($ecAdapter);
+        $builder->spendOutput($spendTx, 0);
+        $builder->payToAddress($pk1->getAddress(), 50);
+        try {
+            $builder->getInputState(0);
+        } catch (BuilderNoInputState $e) {
+            $this->assertTrue(!!$e);
+        }
+
+        $builder = new TransactionBuilder($ecAdapter, TransactionFactory::fromHex($builder->getTransaction()->getHex()), $ecAdapter);
+
+        $builder->createInputState(0, $outputScript, $redeemScript);
+        $is = $builder->getInputState(0);
+        $this->assertInstanceOf($this->txBldrStateType, $is);
+        $this->assertEquals(InputClassifier::MULTISIG, $is->getScriptType());
+        $this->assertEquals(InputClassifier::PAYTOSCRIPTHASH, $is->getPrevOutType());
+        $this->assertEquals(0, $is->getSigCount());
+
+        // Check input is correctly set up
+        $i = $builder->getTransaction()->getInputs()->getInput(0);
+        $this->assertEquals($spendTx->getTransactionId(), $i->getTransactionId());
+        $this->assertEquals(0, $i->getVout());
+        $this->assertEquals(new Script(), $i->getScript());
+
+        // Check output is correctly set up
+        $o = $builder->getTransaction()->getOutputs()->getOutput(0);
+        $this->assertEquals(50, $o->getValue());
+        $this->assertEquals(ScriptFactory::scriptPubKey()->payToPubKeyHash($pk1->getPublicKey()), $o->getScript());
+
+        // Have the builder sign
+        $builder->signInputWithKey($pk1, $outputScript, 0, $redeemScript);
+
+        // Import the 1/2 signed transaction
+        $builder = new TransactionBuilder($ecAdapter, TransactionFactory::fromHex($builder->getTransaction()->getHex()), $ecAdapter);
+        $builder->signInputWithKey($pk2, $outputScript, 0, $redeemScript);
+        $this->assertEquals(2, $builder->getInputState(0)->getSigCount());
+        $hex = $builder->getTransaction()->getHex();
+        $t = TransactionFactory::fromHex($hex);
+
+        // Check that all signatures and state can be extracted.
+        $builder = new TransactionBuilder($ecAdapter, $t);
+        $builder->createInputState(0, $outputScript, $redeemScript);
+        $this->assertEquals(2, $builder->getInputState(0)->getSigCount());
+        $this->assertTrue($builder->getInputState(0)->isFullySigned());
     }
 }
