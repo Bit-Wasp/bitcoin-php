32d34973e2ad506d4c557f1a929a5e83957dec8c
Thomas Kerin <thomas@bitwasp.co>
2015-05-01 15:38:12 +0100

    Tidy SignedMessage create example

--- examples/signedmessage.create.php
+++ examples/signedmessage.create.php
@@ -3,15 +3,14 @@ require "../vendor/autoload.php";
 
 use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\MessageSigner\MessageSigner;
-
-$message = 'hi';
-$privateKey = \BitWasp\Bitcoin\Key\PrivateKeyFactory::create(true);
-
+use BitWasp\Bitcoin\Key\PrivateKeyFactory;
 
 $ec = Bitcoin::getEcAdapter();
+$privateKey = PrivateKeyFactory::create(true);
+
+$message = 'hi';
+
 $signer = new MessageSigner($ec);
 $signed = $signer->sign($message, $privateKey);
-echo "Signed by " . $privateKey->getAddress()->getAddress() . "\n";
 
-$output = $signed->getBuffer()->getBinary();
-echo $output . "\n";
+echo sprintf("Signed by %s\n%s\n", $privateKey->getAddress()->getAddress(), $signed->getBuffer()->getBinary());
