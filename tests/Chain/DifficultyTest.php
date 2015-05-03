1946a80d87218547edef637f96e425b54b2e06c4
Thomas Kerin <thomas@bitwasp.co>
2015-05-03 15:02:34 +0100

    Add test for work of genesis block

--- tests/Chain/DifficultyTest.php
+++ tests/Chain/DifficultyTest.php
@@ -2,25 +2,51 @@
 
 namespace BitWasp\Bitcoin\Tests\Chain;
 
+use BitWasp\Bitcoin\Chain\DifficultyInterface;
+use BitWasp\Bitcoin\Tests\AbstractTestCase;
 use BitWasp\Buffertools\Buffer;
 use BitWasp\Bitcoin\Chain\Difficulty;
 use BitWasp\Bitcoin\Bitcoin;
 
-class DifficultyTest extends \PHPUnit_Framework_TestCase
+class DifficultyTest extends AbstractTestCase
 {
+    /**
+     * @var \BitWasp\Bitcoin\Math\Math
+     */
     protected $math;
-    protected $targetHash;
-    protected $bits;
 
     /**
-     *
+     * @var string
      */
+    protected $targetHash;
+
+    /**
+     * @var Buffer
+     */
+    protected $bits;
+
     public function __construct()
     {
         $this->math = Bitcoin::getMath();
-        ;
         $this->bits = Buffer::hex('1d00ffff');
         $this->targetHash = '00000000ffff0000000000000000000000000000000000000000000000000000';
+    }
+
+    public function testGetWork()
+    {
+        $vectors = [
+            [
+                Buffer::hex('1d00ffff'),
+                '4295032833'
+            ]
+        ];
+
+        $difficulty = new Difficulty($this->math, $this->bits);
+
+        foreach ($vectors as $v) {
+            $this->assertEquals($v[1], $difficulty->getWork($v[0]));
+        }
+
     }
 
     public function testDefaultLowestDifficulty()
@@ -38,6 +64,8 @@ class DifficultyTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($this->bits, $difficulty->lowestBits());
         $this->assertEquals($this->math->hexDec($this->targetHash), $difficulty->getMaxTarget());
     }
+
+
 
     public function testSetLowestDifficulty()
     {
