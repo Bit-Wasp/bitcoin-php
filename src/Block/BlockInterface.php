87e3efaf9eae04398da4f8dde461e8b0d5fa2da5
Thomas Kerin <thomas@bitwasp.co>
2015-05-08 18:59:55 +0100

    phpdoc in BlockInterface

--- src/Block/BlockInterface.php
+++ src/Block/BlockInterface.php
@@ -11,16 +11,22 @@ interface BlockInterface extends SerializableInterface
     const MAX_BLOCK_SIZE = 1000000;
 
     /**
+     * Get the header of this block.
+     *
      * @return BlockHeaderInterface
      */
     public function getHeader();
 
     /**
-     * @return mixed
+     * Calculate the merkle root of the transactions in the block.
+     *
+     * @return string
      */
     public function getMerkleRoot();
 
     /**
+     * Return the TransactionCollection from the block.
+     * 
      * @return TransactionCollection
      */
     public function getTransactions();
