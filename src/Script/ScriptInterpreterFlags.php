3630d3a0aa505e5944844706b2b64b33c59d2ae1
Thomas Kerin <thomas@bitwasp.co>
2015-05-09 16:05:02 +0100

    Add actual flags to ScriptInterpreterFlags - that class needs a rework

--- src/Script/ScriptInterpreterFlags.php
+++ src/Script/ScriptInterpreterFlags.php
@@ -4,6 +4,24 @@ namespace BitWasp\Bitcoin\Script;
 
 class ScriptInterpreterFlags
 {
+
+    const SCRIPT_VERIFY_NONE = 0;
+    // 1 << 0
+    const SCRIPT_VERIFY_P2SH = 1;
+
+    // 1 << 1
+    const SCRIPT_VERIFY_STRICTENC = 2;
+
+    // 1 < 2
+    const SCRIPT_VERIFY_DERSIG = 4;
+
+    const SCRIPT_VERIFY_LOW_S = 8;
+
+    const SCRIPT_VERIFY_NULL_DUMMY = 16;
+    const SCRIPT_VERIFY_SIGPUSHONLY = 32;
+    const SCRIPT_VERIFY_MINIMALDATA = 64;
+    const SCRIPT_VERIFY_DISCOURAGE_UPGARDABLE_NOPS = 128;
+
     public $discourageUpgradableNOPS = false;
     public $maxBytes = 10000;
     public $maxElementSize = 520;
@@ -15,9 +33,9 @@ class ScriptInterpreterFlags
 
     public function __construct()
     {
-        
+
     }
-    
+
     public static function defaults()
     {
         $flags = new self();
