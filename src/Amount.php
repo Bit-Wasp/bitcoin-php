b314e7d5861c1af962baee129b96806069309877
Thomas Kerin <thomas@bitwasp.co>
2015-05-20 00:54:07 +0100

    Tweak Amount class, and add tests

--- src/Amount.php
+++ src/Amount.php
@@ -2,16 +2,17 @@
 
 namespace BitWasp\Bitcoin;
 
-
 class Amount
 {
+    const COIN = 100000000;
+
     /**
      * @param int|string $satoshis
      * @return double
      */
     public function toBTC($satoshis)
     {
-        return bcdiv((string)$satoshis, 100000000, 8);
+        return bcdiv((string)$satoshis, self::COIN, 8);
     }
 
     /**
@@ -20,6 +21,6 @@ class Amount
      */
     public function toSatoshis($double)
     {
-        return bcmul(sprintf("%.8f", (float)$double), 10000000, 0);
+        return bcmul((float)$double, self::COIN, 0);
     }
 }
