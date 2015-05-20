cc7436ad03d7649c4f0f405b21b50b6f61c67227
Thomas Kerin <thomas@bitwasp.co>
2015-05-20 04:11:51 +0100

    Add $flags int to ScriptInterpreterFlags __construct()  - second parameter determines whether interpreter should fail on disabled opcodes
    Add checkFlag($int) to ScriptInterpreterFlags, and checkDisabledOpcodes()

--- src/Script/ScriptInterpreterFlags.php
+++ src/Script/ScriptInterpreterFlags.php
@@ -6,6 +6,7 @@ class ScriptInterpreterFlags
 {
 
     const VERIFY_NONE = 0;
+
     // 1 << 0
     const VERIFY_P2SH = 1;
 
@@ -21,6 +22,7 @@ class ScriptInterpreterFlags
     const VERIFY_SIGPUSHONLY = 32;
     const VERIFY_MINIMALDATA = 64;
     const VERIFY_DISCOURAGE_UPGARDABLE_NOPS = 128;
+    const VERIFY_CLEAN_STACK = 256;
 
     // consensus derived
     public $verifyP2SH = false;
@@ -30,20 +32,41 @@ class ScriptInterpreterFlags
     public $verifySigPushonly = false;
     public $verifyMinimalPushdata = false;
     public $discourageUpgradableNOPS = false;
+    public $verifyCleanStack = false;
 
     // globals
     public $maxBytes = 10000;
     public $maxElementSize = 520;
     public $checkDisabledOpcodes = false;
 
-    public function __construct()
-    {
+    private $flags;
 
+    public function __construct($flags, $checkDisabledOpcodes = false)
+    {
+        if (!is_bool($checkDisabledOpcodes)) {
+            throw new \InvalidArgumentException('CheckDisabledOpcodes must be a boolean');
+        }
+
+        $this->checkDisabledOpcodes = $checkDisabledOpcodes;
+    }
+
+    public function checkDisabledOpcodes()
+    {
+        return $this->checkDisabledOpcodes;
+    }
+
+    public function checkFlag($flags)
+    {
+        return $this->flags & $flags;
     }
 
     public static function defaults()
     {
-        $flags = new self();
+        $flags = new self(
+            self::VERIFY_P2SH | self::VERIFY_STRICTENC | self::VERIFY_DERSIG |
+            self::VERIFY_LOW_S | self::VERIFY_NULL_DUMMY | self:: VERIFY_SIGPUSHONLY |
+            self::VERIFY_DISCOURAGE_UPGARDABLE_NOPS | self::VERIFY_CLEAN_STACK
+        );
         // Set up present settings
         $flags->discourageUpgradableNOPS = true;
         $flags->checkDisabledOpcodes     = true;
@@ -53,6 +76,7 @@ class ScriptInterpreterFlags
         $flags->verifyDERSignatures      = true;
         $flags->verifyStrictEncoding     = true;
         $flags->verifyP2SH               = true;
+
         return $flags;
     }
 }
