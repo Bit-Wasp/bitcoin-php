2a2810f0fcfee4a31751674d2bbbc2b34f68aee6
Thomas Kerin <thomas@bitwasp.co>
2015-06-20 16:20:44 +0100

    Mempool Network Serializer test

--- tests/Network/Messages/MemPoolTest.php
+++ tests/Network/Messages/MemPoolTest.php
@@ -2,6 +2,10 @@
 
 namespace BitWasp\Bitcoin\Test\Network\Messages;
 
+use BitWasp\Bitcoin\Bitcoin;
+use BitWasp\Bitcoin\Crypto\Random\Random;
+use BitWasp\Bitcoin\Network\MessageFactory;
+use BitWasp\Bitcoin\Serializer\Network\NetworkMessageSerializer;
 use BitWasp\Buffertools\Buffer;
 use BitWasp\Bitcoin\Network\Messages\MemPool;
 use BitWasp\Bitcoin\Tests\AbstractTestCase;
@@ -13,5 +17,14 @@ class MemPoolTest extends AbstractTestCase
         $mem = new MemPool();
         $this->assertSame('mempool', $mem->getNetworkCommand());
         $this->assertEquals(new Buffer(), $mem->getBuffer());
+    }
+
+    public function testNetworkSerializer()
+    {
+        $mem = new MemPool();
+        $serializer = new NetworkMessageSerializer(Bitcoin::getDefaultNetwork());
+
+        $parsed = $serializer->parse($mem->getNetworkMessage()->getBuffer())->getPayload();
+        $this->assertEquals($mem, $parsed);
     }
 }
