2ab565c55f83631cc8d29ae1fc82803931119234
Thomas Kerin <thomas@bitwasp.co>
2015-06-20 15:16:29 +0100

    Have Alert extend NetworkSerializable

--- src/Network/Messages/Alert.php
+++ src/Network/Messages/Alert.php
@@ -2,12 +2,13 @@
 
 namespace BitWasp\Bitcoin\Network\Messages;
 
+use BitWasp\Bitcoin\Network\NetworkSerializable;
 use BitWasp\Bitcoin\Network\Structure\AlertDetail;
 use BitWasp\Bitcoin\Serializer\Network\Message\AlertSerializer;
 use BitWasp\Bitcoin\Serializer\Network\Structure\AlertDetailSerializer;
 use BitWasp\Bitcoin\Signature\SignatureInterface;
 
-class Alert
+class Alert extends NetworkSerializable
 {
     /**
      * @var AlertDetail
@@ -27,6 +28,14 @@ class Alert
     {
         $this->alert = $alert;
         $this->signature = $signature;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNetworkCommand()
+    {
+        return 'alert';
     }
 
     /**
