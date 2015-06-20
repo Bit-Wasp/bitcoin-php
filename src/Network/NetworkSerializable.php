08457c7a45860d0a264739995d034d23c79b029e
Thomas Kerin <thomas@bitwasp.co>
2015-06-20 17:25:33 +0100

    NetworkSerializable: Typehint against NetworkInterface instead of concrete instance

--- src/Network/NetworkSerializable.php
+++ src/Network/NetworkSerializable.php
@@ -4,18 +4,18 @@ namespace BitWasp\Bitcoin\Network;
 
 use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Serializable;
-use BitWasp\Bitcoin\Serializer\Network\NetworkMessageSerializer;
 
 abstract class NetworkSerializable extends Serializable implements NetworkSerializableInterface
 {
     /**
-     * @param Network $network
+     * @param NetworkInterface $network
      * @return NetworkMessage
      */
-    public function getNetworkMessage(Network $network = null)
+    public function getNetworkMessage(NetworkInterface $network = null)
     {
-        $network = $network ?: Bitcoin::getNetwork();
-        $message = new NetworkMessage($network, $this);
-        return $message;
+        return new NetworkMessage(
+            $network ?: Bitcoin::getNetwork(),
+            $this
+        );
     }
 }
