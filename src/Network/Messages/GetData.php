e2663ebfed157eaca74ce3a837382ee68eba5833
Thomas Kerin <thomas@bitwasp.co>
2015-06-21 00:58:10 +0100

    apply same to GetData message

--- src/Network/Messages/GetData.php
+++ src/Network/Messages/GetData.php
@@ -2,75 +2,17 @@
 
 namespace BitWasp\Bitcoin\Network\Messages;
 
-use BitWasp\Bitcoin\Network\NetworkSerializable;
-use BitWasp\Bitcoin\Network\Structure\InventoryVector;
 use BitWasp\Bitcoin\Serializer\Network\Message\GetDataSerializer;
 use BitWasp\Bitcoin\Serializer\Network\Structure\InventoryVectorSerializer;
-use BitWasp\Buffertools\Parser;
-use InvalidArgumentException;
 
-class GetData extends NetworkSerializable implements \Countable
+class GetData extends AbstractInventory implements \Countable
 {
-    /**
-     * @var InventoryVector[]
-     */
-    private $vectors = [];
-
-    /**
-     * @param InventoryVector[] $vectors
-     */
-    public function __construct(array $vectors = [])
-    {
-        foreach ($vectors as $vector) {
-            $this->addItem($vector);
-        }
-    }
-
     /**
      * @return string
      */
     public function getNetworkCommand()
     {
         return 'getdata';
-    }
-
-    /**
-     * @return int
-     */
-    public function count()
-    {
-        return count($this->vectors);
-    }
-
-    /**
-     * @return \BitWasp\Bitcoin\Network\Structure\InventoryVector[]
-     */
-    public function getItems()
-    {
-        return $this->vectors;
-    }
-
-    /**
-     * @param int|string $index
-     * @return InventoryVector
-     */
-    public function getItem($index)
-    {
-        if (false === isset($this->vectors[$index])) {
-            throw new InvalidArgumentException('No item exists at that index');
-        }
-
-        return $this->vectors[$index];
-    }
-
-    /**
-     * @param InventoryVector $vector
-     * @return $this
-     */
-    public function addItem(InventoryVector $vector)
-    {
-        $this->vectors[] = $vector;
-        return $this;
     }
 
     /**
