3177c4623fb444b0eae139daac5d7ba3ae5a9bdb
Thomas Kerin <thomas@bitwasp.co>
2015-06-21 00:55:27 +0100

    Use an abstract class for network messages which carry InventoryVectors[], and only differ in name

---
+++ src/Network/Messages/AbstractInventory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace BitWasp\Bitcoin\Network\Messages;
+
+
+use BitWasp\Bitcoin\Network\NetworkSerializable;
+use BitWasp\Bitcoin\Network\Structure\InventoryVector;
+
+abstract class AbstractInventory extends NetworkSerializable implements \Countable
+{
+    /**
+     * @var InventoryVector[]
+     */
+    private $items;
+
+    /**
+     * @param array $vector
+     */
+    public function __construct(array $vector)
+    {
+        foreach ($vector as $item) {
+            $this->addItem($item);
+        }
+    }
+
+    /**
+     * @param InventoryVector $item
+     */
+    private function addItem(InventoryVector $item)
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    /**
+     * @return \BitWasp\Bitcoin\Network\Structure\InventoryVector[]
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param int $index
+     * @return InventoryVector
+     */
+    public function getItem($index)
+    {
+        if (false === isset($this->items[$index])) {
+            throw new \InvalidArgumentException('No item found at that index');
+        }
+
+        return $this->items[$index];
+    }
+}
--- src/Network/Messages/NotFound.php
+++ src/Network/Messages/NotFound.php
@@ -2,74 +2,17 @@
 
 namespace BitWasp\Bitcoin\Network\Messages;
 
-use BitWasp\Bitcoin\Network\NetworkSerializable;
-use BitWasp\Bitcoin\Network\Structure\InventoryVector;
 use BitWasp\Bitcoin\Serializer\Network\Message\NotFoundSerializer;
-use BitWasp\Bitcoin\Serializer\Network\Structure\InventoryVectorSerializer;
-use BitWasp\Buffertools\Parser;
+use BitWasp\Bitcoin\Serializer\Network\Structure\InventoryVectorSerializer;;
 
-class NotFound extends NetworkSerializable implements \Countable
+class NotFound extends AbstractInventory implements \Countable
 {
-    /**
-     * @var InventoryVector[]
-     */
-    private $vectors = [];
-
-    /**
-     * @param InventoryVector[] $vectors
-     */
-    public function __construct(array $vectors = [])
-    {
-        foreach ($vectors as $vector) {
-            $this->addItem($vector);
-        }
-    }
-
     /**
      * @return string
      */
     public function getNetworkCommand()
     {
         return 'notfound';
-    }
-
-    /**
-     * @return int
-     */
-    public function count()
-    {
-        return count($this->vectors);
-    }
-
-    /**
-     * @return \BitWasp\Bitcoin\Network\Structure\InventoryVector[]
-     */
-    public function getItems()
-    {
-        return $this->vectors;
-    }
-
-    /**
-     * @param int $index
-     * @return InventoryVector
-     */
-    public function getItem($index)
-    {
-        if (false === isset($this->vectors[$index])) {
-            throw new \InvalidArgumentException('No item found at that index');
-        }
-
-        return $this->vectors[$index];
-    }
-
-    /**
-     * @param InventoryVector $vector
-     * @return $this
-     */
-    public function addItem(InventoryVector $vector)
-    {
-        $this->vectors[] = $vector;
-        return $this;
     }
 
     /**
