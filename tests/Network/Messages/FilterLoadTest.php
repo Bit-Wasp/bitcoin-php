418d59bf3732f3b6de814725eff65c8c1d70dcb8
Thomas Kerin <afk11@users.noreply.github.com>
2015-06-26 05:24:18 +0100

    new file

---
+++ tests/Network/Messages/FilterLoadTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BitWasp\Bitcoin\Test\Network\Messages;
+
+
+use BitWasp\Bitcoin\Crypto\Random\Random;
+use BitWasp\Bitcoin\Tests\AbstractTestCase;
+use Mdanter\Ecc\Message\MessageFactory;
+
+class FilterLoadTest extends AbstractTestCase
+{
+    public function testNetworkSerialize()
+    {
+        $math = $this->safeMath();
+        $factory = new MessageFactory($math, new Random());
+
+    }
+}
