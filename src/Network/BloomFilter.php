5e877206aff7fd1fb4260045d8572fa37a524f44
Thomas Kerin <afk11@users.noreply.github.com>
2015-06-26 02:02:32 +0100

    Add initial BloomFilter class

---
+++ src/Network/BloomFilter.php
@@ -0,0 +1,309 @@
+<?php
+
+namespace BitWasp\Bitcoin\Network;
+
+
+use BitWasp\Bitcoin\Crypto\Hash;
+use BitWasp\Bitcoin\Flags;
+use BitWasp\Bitcoin\Math\Math;
+use BitWasp\Bitcoin\Script\Classifier\ScriptClassifierInterface;
+use BitWasp\Bitcoin\Script\ScriptFactory;
+use BitWasp\Bitcoin\Transaction\TransactionInterface;
+use BitWasp\Buffertools\Buffer;
+
+class BloomFilter
+{
+    const LN2SQUARED = '0.4804530139182014246671025263266649717305529515945455';
+    const LN2 = '0.6931471805599453094172321214581765680755001343602552';
+    const MAX_HASH_FUNCS = '50';
+    const MAX_FILTER_SIZE = 36000; // bytes
+    const TWEAK_START = '4221880213'; // 0xFBA4C795
+
+    const UPDATE_NONE = 0;
+    const UPDATE_ALL = 1;
+    const UPDATE_P2PUBKEY_ONLY = 2;
+    const UPDATE_MASK = 3;
+
+    /**
+     * @var Math
+     */
+    private $math;
+
+    /**
+     * @var float
+     */
+    private $numHashFuncs;
+
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * @var bool
+     */
+    private $isEmpty = false;
+
+    /**
+     * @var bool
+     */
+    private $isFull = false;
+
+    /**
+     * @param Math $math
+     * @param int $nElements
+     * @param int $nFpRate
+     * @param int|string $nTweak
+     * @param Flags $flags
+     */
+    public function __construct(Math $math, $nElements, $nFpRate, $nTweak, Flags $flags)
+    {
+        $this->math = $math;
+        $size = self::idealSize($nElements, $nFpRate);
+        for ($i = 0; $i < $size; $i++) {
+            $this->data[$i] = '0';
+        }
+
+        $this->numHashFuncs = self::idealNumHashFuncs($size, $nElements);
+        $this->tweak = $nTweak;
+        $this->flags = $flags;
+    }
+
+    /**
+     * @param int $nElements
+     * @param double $fpRate
+     * @return float
+     */
+    public static function idealSize($nElements, $fpRate)
+    {
+        return floor(
+            bcdiv(
+                min(
+                    bcmul(
+                        bcmul(
+                            bcdiv(
+                                -1,
+                                self::LN2SQUARED
+                            ),
+                            $nElements
+                        ),
+                        log($fpRate)
+                    ),
+                    bcmul(
+                        self::MAX_FILTER_SIZE,
+                        8
+                    )
+                ),
+                8
+            )
+        );
+    }
+
+    /**
+     * @param int $filterSize
+     * @param int $nElements
+     * @return float
+     */
+    public static function idealNumHashFuncs($filterSize, $nElements)
+    {
+        return floor(
+            min(
+                bcmul(
+                    bcdiv(
+                        bcmul(
+                            $filterSize,
+                            8
+                        ),
+                        $nElements
+                    ),
+                    self::LN2
+                )
+            )
+        );
+    }
+
+    /**
+     * @param int $nHashNum
+     * @param Buffer $data
+     * @return Int
+     */
+    public function hash($nHashNum, Buffer $data)
+    {
+        $math = $this->math;
+
+        return $math->mod(
+            Hash::murmur3(
+                $data,
+                ($nHashNum * self::TWEAK_START + $this->tweak) & 0xffffffff
+            )->getInt(),
+            count($this->data) * 8
+        );
+    }
+
+    /**
+     * @param Buffer $data
+     */
+    public function insertData(Buffer $data)
+    {
+        if ($this->isFull) {
+            return;
+        }
+
+        for ($i = 0; $i < $this->numHashFuncs; $i++)
+        {
+            $index = $this->hash($i, $data);
+            $this->data[$index >> 3] |= (1 << (7 & $index));
+        }
+
+        $this->isEmpty = false;
+    }
+
+    /**
+     * @param string $txid
+     * @param int $vout
+     */
+    public function insertOutpoint($txid, $vout)
+    {
+        $this->insertData(new Buffer(pack("H64N", $txid, $vout)));
+    }
+
+    /**
+     * @param string $hash
+     */
+    public function insertHash($hash)
+    {
+        $this->insertData(Buffer::hex($hash, 32));
+    }
+
+    /**
+     * @param Buffer $data
+     * @return bool
+     */
+    public function containsData(Buffer $data)
+    {
+        if ($this->isFull) {
+            return true;
+        }
+
+        if ($this->isEmpty) {
+            return false;
+        }
+
+        for ($i = 0; $i < $this->numHashFuncs; $i++) {
+            $index = $this->hash($i, $data);
+            if (!$this->data[$index >> 3] & (1 << (7 & $index))) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $txid
+     * @param int $vout
+     * @return bool
+     */
+    public function containsUtxo($txid, $vout)
+    {
+        return $this->containsData(new Buffer(pack("H64N", $txid, $vout)));
+    }
+
+    /**
+     * @param string $hash
+     * @return bool
+     */
+    public function containsHash($hash)
+    {
+        return $this->containsData(Buffer::hex($hash, 32));
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasAcceptableSize()
+    {
+        return count($this->data) <= self::MAX_FILTER_SIZE && $this->numHashFuncs <= self::MAX_HASH_FUNCS;
+    }
+
+    /**
+     * @param TransactionInterface $tx
+     * @return bool
+     */
+    public function isRelevantAndUpdate(TransactionInterface $tx)
+    {
+        $found = false;
+        if ($this->isFull) {
+            return true;
+        }
+
+        if ($this->isEmpty) {
+            return false;
+        }
+
+
+        $txHash = $tx->getTransactionId();
+        if ($this->containsHash($txHash)) {
+            $found = true;
+        }
+
+        $nFlags = $this->flags->getFlags();
+        $outputs = $tx->getOutputs();
+
+        for ($i = 0, $nOutputs = count($outputs); $i < $nOutputs; $i++) {
+            $txOut = $outputs->getOutput($i);
+            $script = $txOut = $txOut->getScript();
+            while ($script->getScriptParser()->next($opcode, $pushdata)) {
+                if ($pushdata instanceof Buffer && $pushdata->getSize() > 0 && $this->containsData($pushdata)) {
+                    $found = true;
+                    if ($nFlags & self::UPDATE_MASK == self::UPDATE_ALL) {
+                        $this->insertOutpoint($txHash, $i);
+                    } else if ($nFlags & self::UPDATE_MASK == self::UPDATE_P2PUBKEY_ONLY) {
+                        $type = ScriptFactory::scriptPubKey()->classify($script);
+                        if ($type == ScriptClassifierInterface::MULTISIG || $type == ScriptClassifierInterface::PAYTOPUBKEY) {
+                            $this->insertOutpoint($txHash, $i);
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($found) {
+            return true;
+        }
+
+        $inputs = $tx->getInputs();
+
+        for ($i = 0, $nInputs = count($inputs); $i < $nInputs; $i++) {
+            $txIn = $inputs->getInput($i);
+            if ($this->containsUtxo($txIn->getTransactionId(), $txIn->getVout())) {
+                return true;
+            }
+
+            $script = $txIn->getScript();
+            while ($script->getScriptParser()->next($opcode, $pushdata)) {
+                if ($pushdata instanceof Buffer && $pushdata->getSize() > 0 && $this->containsData($pushdata)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     */
+    public function updateEmptyFull()
+    {
+        $full = true;
+        $empty = true;
+        for ($i = 0, $size = count($this->data); $i < $size; $i++) {
+            $full &= ($this->data[$i] == 0xff);
+            $empty &= ($this->data[$i] == 0x0);
+        }
+
+        $this->isFull = $full;
+        $this->isEmpty = $empty;
+    }
+}
