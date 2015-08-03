1af69b189bb7ecc58a9947311872cb05f83b63ed
Thomas Kerin <afk11@users.noreply.github.com>
2015-08-03 17:23:30 +0100

    Finish adding new functions to Secp256k1Stubs

--- stubs/Secp256k1Stubs.php
+++ stubs/Secp256k1Stubs.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace {
-
+    
     /**
-     * @param int $flags
+     * Create a Secp256k1 context resource
+     *
+     * @param $flags - create a VERIFY (or/and) SIGN context
      * @return resource
      */
     function secp256k1_context_create($flags)
@@ -11,143 +13,185 @@ namespace {
     }
 
     /**
-     * @param resource $context
+     * Destroy a Secp256k1 context resource
+     *
+     * @param resource $secp256k1_context_t - context to destroy
      * @return bool
      */
-    function secp256k1_context_destroy($context)
+    function secp256k1_context_destroy($secp256k1_context_t)
     {
     }
 
     /**
-     * @param resource $context
+     * Clone a Secp256k1 context resource
+     *
+     * @param resource $secp256k1_context_t - context to clone
      * @return resource
      */
-    function secp256k1_context_clone($context)
+    function secp256k1_context_clone($secp256k1_context_t)
     {
     }
 
     /**
-     * @param $context
-     * @return void
-     */
-    function secp256k1_context_randomize($context)
-    {
-    }
-
-    /**
-     * @param resource $context
-     * @param string $msg32
-     * @param string $signature
-     * @param string $publicKey
+     * Updates the context randomization (used only internally for blinding)
+     *
+     * @param resource $secp256k1_context_t
      * @return int
      */
-    function secp256k1_ecdsa_verify($context, $msg32, $signature, $publicKey)
+    function secp256k1_context_randomize($secp256k1_context_t)
     {
     }
 
     /**
-     * @param resource $context
+     * Serializes a secp256k1_ecdsa_signature_t resource as DER into $signatureOut.
+     *
+     * @param resource $secp256k1_context_t
+     * @param resource $secp256k1_ecdsa_signature_t
+     * @param string $signatureOut
+     * @return int
+     */
+    function secp256k1_ecdsa_signature_serialize_der($secp256k1_context_t, $secp256k1_ecdsa_signature_t, &$signatureOut)
+    {
+    }
+
+    /**
+     * Serializes a secp256k1_ecdsa_signature_t resource in compact form, into $signatureOut.
+     * If 1 is returned, the signature was saved. When $recIdOut is set, the recovery ID will
+     * be saved to this variable.
+     *
+     * @param resource $secp256k1_context_t
+     * @param resource $secp256k1_ecdsa_signature_t
+     * @param string $signatureOut
+     * @param int $recIdOut (opt) when set, the recid will be saved here.
+     * @return int
+     */
+    function secp256k1_ecdsa_signature_serialize_compact($secp256k1_context_t, $secp256k1_ecdsa_signature_t, &$signatureOut, &$recIdOut)
+    {
+    }
+
+    /**
+     * Parses a DER signature into a secp256k1_ecdsa_signature_t resource.
+     *
+     * @param resource $secp256k1_context_t
+     * @param string $signatureIn
+     * @param resource $secp256k1_ecdsa_signature_t
+     */
+    function secp256k1_ecdsa_signature_parse_der($secp256k1_context_t, $signatureIn, $secp256k1_ecdsa_signature_t)
+    {
+    }
+
+    /**
+     * Parses a compact signature into a secp256k1_ecdsa_signature_t resource.
+     * Note: If $recIdIn is provided, the resource will support pubkey recovery
+     *
+     * @param resource $secp256k1_context_t
+     * @param string $signatureIn
+     * @param resource $secp256k1_ecdsa_signature_t
+     * @param int $recIdIn (opt) - pass the recid to allow pubkey recovery
+     */
+    function secp256k1_ecdsa_signature_parse_compact($secp256k1_context_t, $signatureIn, $secp256k1_ecdsa_signature_t, $recIdIn)
+    {
+    }
+
+    /**
+     * @param resource $secp256k1_context_t
+     * @param string $msg32
+     * @param resource $secp256k1_ecdsa_signature_t - signature resource
+     * @param resource $secp256k1_pubkey_t - the public key resource
+     * @return int
+     */
+    function secp256k1_ecdsa_verify($secp256k1_context_t, $msg32, $secp256k1_ecdsa_signature_t, $secp256k1_pubkey_t)
+    {
+    }
+
+    /**
+     * @param resource $secp256k1_context_t
      * @param string $msg32
      * @param string $privateKey
-     * @param string $signature
+     * @param resource $secp256k1_ecdsa_signature_t
      * @return int
      */
-    function secp256k1_ecdsa_sign($context, $msg32, $privateKey, $signature)
+    function secp256k1_ecdsa_sign($secp256k1_context_t, $msg32, $privateKey, $secp256k1_ecdsa_signature_t)
     {
     }
 
     /**
-     * @param resource $context
+     * @param resource $secp256k1_context_t
      * @param string $msg32
-     * @param string $privateKey
-     * @param string $signature
-     * @param int $recid
+     * @param resource $secp256k1_ecdsa_signature_t
+     * @param resource $secp256k1_pubkey_t
      * @return int
      */
-    function secp256k1_ecdsa_sign_compact($context, $msg32, $privateKey, $signature, $recid)
+    function secp256k1_ecdsa_recover($secp256k1_context_t, $msg32, $secp256k1_ecdsa_signature_t, $secp256k1_pubkey_t)
     {
     }
 
     /**
-     * @param resource $context
-     * @param string $msg32
-     * @param string $signature
-     * @param int $recoveryId
-     * @param int $compressed
-     * @param string $publicKey
-     * @return int
-     */
-    function secp256k1_ecdsa_recover_compact($context, $msg32, $signature, $recoveryId, $compressed, $publicKey)
-    {
-    }
-
-    /**
-     * @param resource $context
+     * @param resource $secp256k1_context_t
      * @param string $secretKey
-     * @param int $compressed
-     * @param string $publicKey
+     * @param resource $secp256k1_pubkey_t
      * @return int
      */
-    function secp256k1_ec_pubkey_create($context, $secretKey, $compressed, $publicKey)
+    function secp256k1_ec_pubkey_create($secp256k1_context_t, $secretKey, $secp256k1_pubkey_t)
     {
     }
 
     /**
-     * @param resource $context
+     * @param resource $secp256k1_context_t
      * @param string $privateKey
      * @param string $tweak
      * @return int
      */
-    function secp256k1_ec_privkey_tweak_add($context, $privateKey, $tweak)
+    function secp256k1_ec_privkey_tweak_add($secp256k1_context_t, $privateKey, $tweak)
     {
     }
 
     /**
-     * @param resource $context
+     * @param resource $secp256k1_context_t
      * @param string $privateKey
      * @param string $tweak
      * @return int
      */
-    function secp256k1_ec_privkey_tweak_mul($context, $privateKey, $tweak)
+    function secp256k1_ec_privkey_tweak_mul($secp256k1_context_t, $privateKey, $tweak)
     {
     }
 
     /**
-     * @param resource $context
-     * @param string $publicKey
+     * @param resource $secp256k1_context_t
+     * @param resource $secp256k1_pubkey_t
      * @param string $tweak
      * @return int
      */
-    function secp256k1_ec_pubkey_tweak_add($context, $publicKey, $tweak)
+    function secp256k1_ec_pubkey_tweak_add($secp256k1_context_t, $secp256k1_pubkey_t, $tweak)
     {
     }
 
     /**
-     * @param resource $context
-     * @param string $publicKey
+     * @param resource $secp256k1_context_t
+     * @param resource $secp256k1_pubkey_t
      * @param string $tweak
      * @return int
      */
-    function secp256k1_ec_pubkey_tweak_mul($context, $publicKey, $tweak)
+    function secp256k1_ec_pubkey_tweak_mul($secp256k1_context_t, $secp256k1_pubkey_t, $tweak)
     {
     }
 
     /**
-     * @param resource $context
+     * @param resource $secp256k1_context_t
      * @param string $publicKey
      * @return int
      */
-    function secp256k1_ec_pubkey_verify($context, $publicKey)
+    function secp256k1_ec_pubkey_verify($secp256k1_context_t, $publicKey)
     {
 
     }
 
     /**
-     * @param resource $context
+     * @param resource $secp256k1_context_t
      * @param string $secKey
      * @return int
      */
-    function secp256k1_ec_seckey_verify($context, $secKey)
+    function secp256k1_ec_seckey_verify($secp256k1_context_t, $secKey)
     {
     }
 }
