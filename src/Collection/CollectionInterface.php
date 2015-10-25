443b9b806fbb5047f32555c78b9962c19874ebf1
Thomas Kerin <afk11@users.noreply.github.com>
2015-10-25 15:11:56 +0000

    Add get() and all() to CollectionInterface

--- src/Collection/CollectionInterface.php
+++ src/Collection/CollectionInterface.php
@@ -4,5 +4,14 @@ namespace BitWasp\Bitcoin\Collection;
 
 interface CollectionInterface extends \Iterator, \ArrayAccess, \Countable
 {
+    /**
+     * @param int $index
+     * @return mixed
+     */
+    public function get($index);
 
+    /**
+     * @return array
+     */
+    public function all();
 }
