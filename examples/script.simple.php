8b7166c92ee65e55f9e8aa08f0f8a1426bfa67ee
Thomas Kerin <afk11@users.noreply.github.com>
2015-11-24 04:29:48 +0000

    Fix an example since run() visibility is now private

--- examples/script.simple.php
+++ examples/script.simple.php
@@ -4,15 +4,17 @@ require_once "../vendor/autoload.php";
 
 use BitWasp\Bitcoin\Script\ScriptFactory;
 use BitWasp\Bitcoin\Transaction\Transaction;
+use BitWasp\Bitcoin\Script\Script;
 
 $ec = \BitWasp\Bitcoin\Bitcoin::getEcAdapter();
 
-$script = ScriptFactory::create()->op('OP_1')->op('OP_1')->op('OP_ADD')->op('OP_2')->op('OP_EQUALVERIFY')->getScript();
+$scriptSig = ScriptFactory::create()->int(1)->int(1)->getScript();
+$scriptPubKey = ScriptFactory::create()->op('OP_ADD')->int(2)->op('OP_EQUAL')->getScript();
 
-echo "Formed script: " . $script->getHex() . "\n";
-print_r($script->getScriptParser()->parse());
+echo "Formed script: " . $scriptSig->getHex() . " " . $scriptPubKey->getHex() . "\n";
 
 $flags = new \BitWasp\Bitcoin\Flags(0);
-$i = new \BitWasp\Bitcoin\Script\Interpreter\Interpreter($ec, new Transaction(), $flags);
-$result = $i->setScript($script)->run();
+$i = new \BitWasp\Bitcoin\Script\Interpreter\Interpreter($ec, new Transaction, $flags);
+
+$result = $i->verify($scriptSig, $scriptPubKey, 0);
 echo "Script result: " . ($result ? 'true' : 'false') . "\n";
