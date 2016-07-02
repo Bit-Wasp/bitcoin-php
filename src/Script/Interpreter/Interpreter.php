6589b87169f32415463ee4bb29999380e19c4760
Thomas Kerin <me@thomaskerin.io>
2016-07-02 01:13:50 +0100

    CLEANSTACK flags check

--- src/Script/Interpreter/Interpreter.php
+++ src/Script/Interpreter/Interpreter.php
@@ -313,7 +313,8 @@ class Interpreter implements InterpreterInterface
         }
 
         if ($flags & self::VERIFY_CLEAN_STACK) {
-            if (!($flags & self::VERIFY_P2SH && $flags & self::VERIFY_WITNESS)) {
+            if (!($flags & self::VERIFY_P2SH != 0) && ($flags & self::VERIFY_WITNESS != 0)) {
+                echo 'cleanstack flags';
                 return false; // implied flags required
             }
 
