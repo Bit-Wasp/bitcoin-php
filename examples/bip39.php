6e6d9279b11e019a1c3d0d1f5d2712ddedac1175
Ruben de Vries <ruben@rubensayshi.com>
2016-07-07 17:12:54 +0200

    fix examples to work from rootdir too

--- examples/bip39.php
+++ examples/bip39.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 use BitWasp\Bitcoin\Crypto\Random\Random;
 use BitWasp\Bitcoin\Mnemonic\MnemonicFactory;
--- examples/bip70.payment.php
+++ examples/bip70.payment.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 use \BitWasp\Bitcoin\PaymentProtocol\PaymentVerifier;
 use \BitWasp\Bitcoin\PaymentProtocol\PaymentHandler;
