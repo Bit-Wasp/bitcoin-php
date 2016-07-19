da06f6efccc0d902b4622390bf0d2a57cce8b27b
Thomas Kerin <me@thomaskerin.io>
2016-07-19 02:47:33 +0100

    RandomTest - tidy tests file

--- tests/Crypto/Random/RandomTest.php
+++ tests/Crypto/Random/RandomTest.php
@@ -2,18 +2,17 @@
 
 namespace BitWasp\Bitcoin\Tests\Crypto\Random;
 
-class RandomTest extends \PHPUnit_Framework_TestCase
-{
-    /**
-     * @var string
-     */
-    protected $bufferType = 'BitWasp\Buffertools\Buffer';
+use BitWasp\Bitcoin\Crypto\Random\Random;
+use BitWasp\Bitcoin\Tests\AbstractTestCase;
+use BitWasp\Buffertools\Buffer;
 
+class RandomTest extends AbstractTestCase
+{
     public function testBytes()
     {
-        $random = new \BitWasp\Bitcoin\Crypto\Random\Random;
+        $random = new Random();
         $bytes  = $random->bytes(32);
-        $this->assertInstanceOf($this->bufferType, $bytes);
+        $this->assertInstanceOf(Buffer::class, $bytes);
         $this->assertEquals(32, $bytes->getSize());
     }
 }
