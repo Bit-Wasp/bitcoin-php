e1ea7206655b736e0da82ee38e14aece03c40764
Thomas Kerin <me@thomaskerin.io>
2016-07-20 13:47:49 +0100

    MultisigTest: avoid random keys

--- tests/Script/ScriptInfo/MultisigTest.php
+++ tests/Script/ScriptInfo/MultisigTest.php
@@ -3,22 +3,18 @@
 namespace BitWasp\Bitcoin\Tests\Script\ScriptInfo;
 
 use BitWasp\Bitcoin\Key\PrivateKeyFactory;
+use BitWasp\Bitcoin\Key\PublicKeyFactory;
 use BitWasp\Bitcoin\Script\Classifier\OutputClassifier;
 use BitWasp\Bitcoin\Script\ScriptFactory;
 use BitWasp\Bitcoin\Script\ScriptInfo\Multisig;
-use BitWasp\Bitcoin\Script\ScriptInfo\PayToPubkey;
-use BitWasp\Bitcoin\Script\ScriptInfo\PayToPubkeyHash;
 use BitWasp\Bitcoin\Tests\AbstractTestCase;
 
 class MultisigTest extends AbstractTestCase
 {
     public function testMethods()
     {
-        $priv = PrivateKeyFactory::create();
-        $pub = $priv->getPublicKey();
-
-        $otherpriv = PrivateKeyFactory::create();
-        $otherpub = $otherpriv->getPublicKey();
+        $pub = PublicKeyFactory::fromHex('045b81f0017e2091e2edcd5eecf10d5bdd120a5514cb3ee65b8447ec18bfc4575c6d5bf415e54e03b1067934a0f0ba76b01c6b9ab227142ee1d543764b69d901e0');
+        $otherpub = $pub->tweakAdd(gmp_init(1));
 
         $script = ScriptFactory::scriptPubKey()->multisig(2, [$pub, $otherpub]);
         $classifier = new OutputClassifier();
@@ -30,7 +26,7 @@ class MultisigTest extends AbstractTestCase
         $this->assertTrue($info->checkInvolvesKey($pub));
         $this->assertTrue($info->checkInvolvesKey($otherpub));
 
-        $unrelatedPub = PrivateKeyFactory::create()->getPublicKey();
+        $unrelatedPub = $otherpub->tweakAdd(gmp_init(1));
         $this->assertFalse($info->checkInvolvesKey($unrelatedPub));
 
     }
