58d69335e29ade5e211a7b6dfce19c7e3e3f3c65
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-04-27 22:25:36 +0200

    does what we want..

--- src/Script/Path/ScriptBranch.php
+++ src/Script/Path/ScriptBranch.php
@@ -2,19 +2,28 @@
 
 namespace BitWasp\Bitcoin\Script\Path;
 
-
 use BitWasp\Bitcoin\Script\ScriptFactory;
 use BitWasp\Bitcoin\Script\ScriptInterface;
 
 class ScriptBranch
 {
-    public function __construct(array $branch, array $segments, ScriptInterface $script)
+    /**
+     * ScriptBranch constructor.
+     * @param ScriptInterface $fullScript
+     * @param bool[] $branch
+     * @param array[] $segments
+     * @param ScriptInterface $script
+     */
+    public function __construct(ScriptInterface $fullScript, array $branch, array $segments, ScriptInterface $script)
     {
         $this->script = $script;
         $this->segments = $segments;
         $this->branch = $branch;
     }
 
+    /**
+     * @return array
+     */
     public function __debugInfo()
     {
         $m = [];
@@ -31,5 +40,21 @@ class ScriptBranch
             'branch' => implode(", ", $path),
             'segments' => $m,
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getSignSteps()
+    {
+        $steps = [];
+        foreach ($this->segments as $segment) {
+            if (count($segment) === 1 && $segment[0]->isLogical()) {
+                
+            } else {
+                $steps[] = $segment;
+            }
+        }
+        return $steps;
     }
 }
