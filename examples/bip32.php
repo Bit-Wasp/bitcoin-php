1883e24925ade7056722826a60752cb9a8dc088a
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-05-25 12:38:30 +0200

    fix illustration in bip32 example

--- examples/bip32.php
+++ examples/bip32.php
@@ -8,7 +8,11 @@ require __DIR__ . "/../vendor/autoload.php";
 $math = Bitcoin::getMath();
 $network = Bitcoin::getNetwork();
 
+// By default, this example produces random keys.
 $master = HierarchicalKeyFactory::generateMasterKey();
+
+// To restore from an existing xprv/xpub:
+//$master = HierarchicalKeyFactory::fromExtended("xprv9s21ZrQH143K4Se1mR27QkNkLS9LSarRVFQcopi2mcomwNPDaABdM1gjyow2VgrVGSYReepENPKX2qiH61CbixpYuSg4fFgmrRtk6TufhPU");
 echo "Master key (m)\n";
 echo "   " . $master->toExtendedPrivateKey($network) . "\n";
 echo "   Address: " . $master->getPublicKey()->getAddress()->getAddress() . "\n\n";
@@ -24,6 +28,6 @@ echo "   Address: " . $key2->getPublicKey()->getAddress()->getAddress() . "\n\n"
 
 echo "Directly derive path\n";
 
-$sameKey2 = $master->derivePath("0'/999999");
+$sameKey2 = $master->derivePath("0/999999");
 echo " - m/0'/999999 " . $sameKey2->toExtendedPublicKey() . "\n";
 echo "   Address: " . $sameKey2->getPublicKey()->getAddress()->getAddress() . "\n\n";
