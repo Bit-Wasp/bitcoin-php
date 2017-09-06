b133a45b8f754677c2d2fe1c1c24fae276f7c6c3
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-09-06 20:48:53 +0200

    test two simple cases in getAstForLogicalOps

---
+++ tests/Script/Branch/BranchInterpreterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BitWasp\Bitcoin\Tests\Script\Branch;
+
+
+use BitWasp\Bitcoin\Script\Opcodes;
+use BitWasp\Bitcoin\Script\Path\BranchInterpreter;
+use BitWasp\Bitcoin\Script\ScriptFactory;
+use BitWasp\Bitcoin\Tests\AbstractTestCase;
+
+class BranchInterpreterTest extends AbstractTestCase
+{
+    public function testDetectsUnexpectedEndIf()
+    {
+        $script = ScriptFactory::sequence([
+            Opcodes::OP_ENDIF,
+        ]);
+
+        $bi = new BranchInterpreter();
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage("Unexpected ENDIF, current scope had no parent");
+
+        $bi->getAstForLogicalOps($script);
+    }
+
+    public function testDetectsUnexpectedElse()
+    {
+        $script = ScriptFactory::sequence([
+            Opcodes::OP_ELSE,
+        ]);
+
+        $bi = new BranchInterpreter();
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage("Unexpected ELSE, current scope had no parent");
+
+        $bi->getAstForLogicalOps($script);
+    }
+}
