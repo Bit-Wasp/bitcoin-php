fd86e4b97310388be0a3db5413cf8c3fb05d50fa
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-09-30 19:41:17 +0200

    Add tests against the bitcoin rpc

--- tests-rpc/AbstractTestCase.php
+++ tests-rpc/AbstractTestCase.php
@@ -4,6 +4,7 @@ namespace BitWasp\Bitcoin\RpcTest;
 
 use BitWasp\Bitcoin\Network\NetworkFactory;
 use BitWasp\Bitcoin\Network\NetworkInterface;
+use BitWasp\Bitcoin\Script\Interpreter\Interpreter;
 use Nbobtc\Command\Command;
 
 abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
@@ -32,6 +33,11 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
      * @var \Nbobtc\Http\Client
      */
     protected $client;
+
+    /**
+     * @var array
+     */
+    private $scriptFlagNames;
 
     /**
      * @var NetworkInterface
@@ -117,4 +123,97 @@ abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
         }
         return $json;
     }
+
+
+    /**
+     * @param string $name
+     * @return array
+     */
+    public function jsonDataFile($name)
+    {
+        $contents = $this->dataFile($name);
+        $decoded = json_decode($contents, true);
+        if (false === $decoded || json_last_error() !== JSON_ERROR_NONE) {
+            throw new \RuntimeException('Invalid JSON file ' . $name);
+        }
+
+        return $decoded;
+    }
+
+    /**
+     * @param string $filename
+     * @return string
+     */
+    public function dataFile($filename)
+    {
+        $contents = file_get_contents($this->dataPath($filename));
+        if (false === $contents) {
+            throw new \RuntimeException('Failed to data file ' . $filename);
+        }
+        return $contents;
+    }
+
+    /**
+     * @param string $file
+     * @return string
+     */
+    public function dataPath($file)
+    {
+        return __DIR__ . '/../tests/Data/' . $file;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function calcMapScriptFlags()
+    {
+        if (null === $this->scriptFlagNames) {
+            $this->scriptFlagNames = [
+                "NONE" => Interpreter::VERIFY_NONE,
+                "P2SH" => Interpreter::VERIFY_P2SH,
+                "STRICTENC" => Interpreter::VERIFY_STRICTENC,
+                "DERSIG" => Interpreter::VERIFY_DERSIG,
+                "LOW_S" => Interpreter::VERIFY_LOW_S,
+                "SIGPUSHONLY" => Interpreter::VERIFY_SIGPUSHONLY,
+                "MINIMALDATA" => Interpreter::VERIFY_MINIMALDATA,
+                "NULLDUMMY" => Interpreter::VERIFY_NULL_DUMMY,
+                "DISCOURAGE_UPGRADABLE_NOPS" => Interpreter::VERIFY_DISCOURAGE_UPGRADABLE_NOPS,
+                "CLEANSTACK" => Interpreter::VERIFY_CLEAN_STACK,
+                "CHECKLOCKTIMEVERIFY" => Interpreter::VERIFY_CHECKLOCKTIMEVERIFY,
+                "CHECKSEQUENCEVERIFY" => Interpreter::VERIFY_CHECKSEQUENCEVERIFY,
+                "WITNESS" => Interpreter::VERIFY_WITNESS,
+                "DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM" => Interpreter::VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM,
+                "MINIMALIF" => Interpreter::VERIFY_MINIMALIF,
+                "NULLFAIL" => Interpreter::VERIFY_NULLFAIL,
+            ];
+        }
+
+        return $this->scriptFlagNames;
+    }
+
+    /**
+     * @param string $string
+     * @return int
+     */
+    public function getScriptFlagsFromString($string)
+    {
+        $mapFlagNames = $this->calcMapScriptFlags();
+        if (strlen($string) === 0) {
+            return Interpreter::VERIFY_NONE;
+        }
+
+        $flags = 0;
+        $words = explode(",", $string);
+        foreach ($words as $word) {
+            if (!isset($mapFlagNames[$word])) {
+                throw new \RuntimeException('Unknown verification flag: ' . $word);
+            }
+
+            $flags |= $mapFlagNames[$word];
+        }
+
+        return $flags;
+    }
+
 }
---
+++ tests-rpc/TransactionTest.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace BitWasp\Bitcoin\RpcTest;
+
+
+use BitWasp\Bitcoin\Key\PrivateKeyFactory;
+use BitWasp\Bitcoin\Network\NetworkFactory;
+use BitWasp\Bitcoin\Script\Interpreter\Interpreter;
+use BitWasp\Bitcoin\Script\ScriptFactory;
+use BitWasp\Bitcoin\Script\ScriptInterface;
+use BitWasp\Bitcoin\Transaction\Factory\SignData;
+use BitWasp\Bitcoin\Transaction\Factory\Signer;
+use BitWasp\Bitcoin\Transaction\Factory\TxBuilder;
+use BitWasp\Bitcoin\Transaction\OutPoint;
+use BitWasp\Bitcoin\Transaction\TransactionFactory;
+use BitWasp\Bitcoin\Transaction\TransactionOutput;
+use BitWasp\Bitcoin\Utxo\Utxo;
+use BitWasp\Buffertools\Buffer;
+
+class TransactionTest extends AbstractTestCase
+{
+    /**
+     * Check tests are being run against regtest
+     */
+    public function testIfRegtest()
+    {
+        $result = $this->makeRpcRequest("getblockchaininfo");
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('result', $result);
+        $this->assertArrayHasKey('chain', $result['result']);
+        $this->assertEquals('regtest', $result['result']['chain']);
+    }
+
+    /**
+     * Produces scripts for signing
+     * @return array
+     */
+    public function getScriptVectors()
+    {
+        return $this->jsonDataFile('signer_fixtures.json')['valid'];
+    }
+
+    /**
+     * Produces ALL vectors
+     * @return array
+     */
+    public function getVectors()
+    {
+        $vectors = [];
+        foreach ($this->getScriptVectors() as $fixture) {
+            $vectors[] = [$this->stripTestData($fixture)];
+        }
+
+        return $vectors;
+    }
+
+    /**
+     * @param array $fixture
+     * @return array
+     */
+    public function stripTestData(array $fixture)
+    {
+        foreach (['hex'] as $key) {
+            if (array_key_exists($key, $fixture)) {
+                unset($fixture[$key]);
+            }
+        }
+        foreach ($fixture['raw']['ins'] as &$input) {
+            unset($input['hash']);
+            unset($input['index']);
+            unset($input['value']);
+        }
+        return $fixture;
+    }
+
+    /**
+     * @param ScriptInterface $script
+     * @param int $value
+     * @return Utxo
+     */
+    public function fundOutput(ScriptInterface $script, $value = 100000000)
+    {
+        $chainInfo = $this->makeRpcRequest('getblockchaininfo');
+        $bestHeight = $chainInfo['result']['blocks'];
+
+        while($bestHeight < 150 || $chainInfo['result']['bip9_softforks']['segwit']['status'] !== 'active') {
+            $this->makeRpcRequest("generate", [1]);
+            $chainInfo = $this->makeRpcRequest('getblockchaininfo');
+            $bestHeight = $chainInfo['result']['blocks'];
+        }
+
+        $builder = new TxBuilder();
+        $builder->output($value, $script);
+        $hex = $builder->get()->getHex();
+
+        $result = $this->makeRpcRequest('fundrawtransaction', [$hex, ['feeRate'=>0.0001]]);
+        $unsigned = $result['result']['hex'];
+        $result = $this->makeRpcRequest('signrawtransaction', [$unsigned]);
+        $signedHex = $result['result']['hex'];
+        $signed = TransactionFactory::fromHex($signedHex);
+        $outIdx = -1;
+        foreach ($signed->getOutputs() as $i => $output) {
+            if ($output->getScript()->equals($script)) {
+                $outIdx = $i;
+            }
+        }
+
+        if ($outIdx === -1) {
+            throw new \RuntimeException("Sanity check failed, should have found the output we funded");
+        }
+
+        $result = $this->makeRpcRequest('sendrawtransaction', [$signedHex]);
+        $txid = $result['result'];
+        $this->makeRpcRequest("generate", [1]);
+
+        return new Utxo(new OutPoint(Buffer::hex($txid), $outIdx), new TransactionOutput($value, $script));
+    }
+
+    /**
+     * @param $fixture
+     * @dataProvider getVectors
+     */
+    public function testCases($fixture)
+    {
+        $defaultPolicy = Interpreter::VERIFY_NONE | Interpreter::VERIFY_P2SH | Interpreter::VERIFY_WITNESS | Interpreter::VERIFY_CHECKLOCKTIMEVERIFY | Interpreter::VERIFY_CHECKSEQUENCEVERIFY;;
+        $txBuilder = new TxBuilder();
+        if (array_key_exists('version', $fixture['raw'])) {
+            $txBuilder->version((int) $fixture['raw']['version']);
+        }
+
+        $totalOut = 12345;
+        foreach ($fixture['raw']['outs'] as $output) {
+            $txBuilder->output($output['value'], ScriptFactory::fromHex($output['script']));
+            $totalOut += $output['value'];
+        }
+
+        /**
+         * @var SignData[] $signDatas
+         * @var Utxo[] $utxos
+         */
+        $signDatas = [];
+        $utxos = [];
+        foreach ($fixture['raw']['ins'] as $input) {
+            $scriptPubKey = ScriptFactory::fromHex($input['scriptPubKey']);
+            if (array_key_exists('value', $input)) {
+                echo "needs value: {$input['value']}\n";
+            }
+
+            $value = array_key_exists('value', $input) ? (int) $input['value'] : $totalOut;
+            $utxo = $this->fundOutput($scriptPubKey, $value);
+
+            $sequence = array_key_exists('sequence', $input) ? (int) $input['sequence'] : 0xffffffff;
+            $txBuilder->spendOutPoint($utxo->getOutPoint(), null, $sequence);
+
+            $signData = new SignData();
+            if (array_key_exists('redeemScript', $input) && "" !== $input['redeemScript']) {
+                $signData->p2sh(ScriptFactory::fromHex($input['redeemScript']));
+            }
+            if (array_key_exists('witnessScript', $input) && "" !== $input['witnessScript']) {
+                $signData->p2wsh(ScriptFactory::fromHex($input['witnessScript']));
+            }
+
+            $policy = array_key_exists('signaturePolicy', $fixture) ? $this->getScriptFlagsFromString($fixture['signaturePolicy']) : $defaultPolicy;
+            $signData->signaturePolicy($policy);
+            $signDatas[] = $signData;
+
+            $utxos[] = $utxo;
+        }
+
+        $txBuilder->locktime(isset($fixture['raw']['locktime']) ? $fixture['raw']['locktime'] : 0);
+
+        $signer = new Signer($txBuilder->get());
+        foreach ($fixture['raw']['ins'] as $i => $input) {
+            $iSigner = $signer->input($i, $utxos[$i]->getOutput(), $signDatas[$i]);
+            foreach ($input['keys'] as $key) {
+                $priv = PrivateKeyFactory::fromWif($key['key'], null, NetworkFactory::bitcoinTestnet());
+                $sigHashType = $key['sigHashType'];
+                $iSigner->sign($priv, $sigHashType);
+            }
+
+            $this->assertTrue($iSigner->isFullySigned());
+        }
+
+        $tx = $signer->get();
+        $result = $this->makeRpcRequest('sendrawtransaction', [$tx->getHex(), true]);
+        $this->assertEquals(null, $result['error']);
+        $txid = $result['result'];
+        $this->assertEquals(64, strlen($txid));
+    }
+}
