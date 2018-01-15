1f933c27460892029c3a62706fd9e46d57d4efc6
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-01-15 12:48:45 +0000

    Block and Transaction serializer - reuse Opcodes

--- src/Block/BlockFactory.php
+++ src/Block/BlockFactory.php
@@ -4,8 +4,13 @@ namespace BitWasp\Bitcoin\Block;
 
 use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Math\Math;
+use BitWasp\Bitcoin\Script\Opcodes;
 use BitWasp\Bitcoin\Serializer\Block\BlockHeaderSerializer;
 use BitWasp\Bitcoin\Serializer\Block\BlockSerializer;
+use BitWasp\Bitcoin\Serializer\Script\ScriptWitnessSerializer;
+use BitWasp\Bitcoin\Serializer\Transaction\OutPointSerializer;
+use BitWasp\Bitcoin\Serializer\Transaction\TransactionInputSerializer;
+use BitWasp\Bitcoin\Serializer\Transaction\TransactionOutputSerializer;
 use BitWasp\Bitcoin\Serializer\Transaction\TransactionSerializer;
 
 class BlockFactory
@@ -17,10 +22,15 @@ class BlockFactory
      */
     public static function fromHex($string, Math $math = null)
     {
+        $opcodes = new Opcodes();
         return (new BlockSerializer(
             $math ?: Bitcoin::getMath(),
             new BlockHeaderSerializer(),
-            new TransactionSerializer()
+            new TransactionSerializer(
+                new TransactionInputSerializer(new OutPointSerializer(), $opcodes),
+                new TransactionOutputSerializer($opcodes),
+                new ScriptWitnessSerializer()
+            )
         ))
             ->parse($string);
     }
