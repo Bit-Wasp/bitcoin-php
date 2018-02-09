3137a93e25fe647759464561f6aa2e658668897b
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-02-09 14:38:12 +0100

    dump logs on failure

--- travis/run_secp256k1_tests.sh
+++ travis/run_secp256k1_tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -e
 if [ "${PHPUNIT_EXT}" = "true" ]; then
-    cd secp256k1-php/secp256k1 && REPORT_EXIT_STATUS=1 make test;
+    cd secp256k1-php/secp256k1;
+    REPORT_EXIT_STATUS=1 make test;
+    if [ $? != "0" ]; then
+        find tests -type f -iname "*.log" -exec cat {} +;
+        exit 1;
+    fi;
 fi
+
+exit 0
