30dbbaef747eca3df044db23027058985887ffe4
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-07-09 22:44:09 +0200

    last invalid slip132 fxn#

--- tests/Key/Deterministic/Slip132/Slip132Test.php
+++ tests/Key/Deterministic/Slip132/Slip132Test.php
@@ -36,25 +36,6 @@ class Slip132Test extends AbstractTestCase
     /**
      * @dataProvider getEcAdapters
      * @param EcAdapterInterface $adapter
-     * @throws \BitWasp\Bitcoin\Exceptions\InvalidNetworkParameter
-     */
-    public function testXpubP2sh(EcAdapterInterface $adapter)
-    {
-        $slip132 = new Slip132(new KeyToScriptHelper($adapter));
-        $registry = new BitcoinRegistry();
-        $prefix = $slip132->p2shP2pkh($registry);
-
-        list ($priv, $pub) = $registry->getPrefixes($prefix->getScriptDataFactory()->getScriptType());
-        $this->assertEquals($pub, $prefix->getPublicPrefix());
-        $this->assertEquals($priv, $prefix->getPrivatePrefix());
-
-        $factory = $prefix->getScriptDataFactory();
-        $this->assertEquals(ScriptType::P2SH . "|" . ScriptType::P2PKH, $factory->getScriptType());
-    }
-
-    /**
-     * @dataProvider getEcAdapters
-     * @param EcAdapterInterface $adapter
      * @throws \BitWasp\Bitcoin\Exceptions\DisallowedScriptDataFactoryException
      * @throws \BitWasp\Bitcoin\Exceptions\InvalidNetworkParameter
      */
