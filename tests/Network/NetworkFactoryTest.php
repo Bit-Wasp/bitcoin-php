9910f8becbc50a74e10590a339f40073f78b6983
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-11-13 00:56:14 +0100

    AbstractBip39Case & NetworkFactoryTest: fix phpdoc

--- tests/Network/NetworkFactoryTest.php
+++ tests/Network/NetworkFactoryTest.php
@@ -5,44 +5,34 @@ declare(strict_types=1);
 namespace BitWasp\Bitcoin\Tests\Network;
 
 use BitWasp\Bitcoin\Network\NetworkFactory;
-use BitWasp\Bitcoin\Network\Networks\Bitcoin;
-use BitWasp\Bitcoin\Network\Networks\BitcoinRegtest;
-use BitWasp\Bitcoin\Network\Networks\BitcoinTestnet;
-use BitWasp\Bitcoin\Network\Networks\Dash;
-use BitWasp\Bitcoin\Network\Networks\DashTestnet;
-use BitWasp\Bitcoin\Network\Networks\Dogecoin;
-use BitWasp\Bitcoin\Network\Networks\DogecoinTestnet;
-use BitWasp\Bitcoin\Network\Networks\Litecoin;
-use BitWasp\Bitcoin\Network\Networks\LitecoinTestnet;
-use BitWasp\Bitcoin\Network\Networks\Viacoin;
-use BitWasp\Bitcoin\Network\Networks\ViacoinTestnet;
+use BitWasp\Bitcoin\Network\Networks;
 use BitWasp\Bitcoin\Tests\AbstractTestCase;
 
 class NetworkFactoryTest extends AbstractTestCase
 {
-    public function getFactoryMethodAndClass()
+    public function getFactoryMethodAndClass(): array
     {
         return [
-            ['bitcoin', Bitcoin::class],
-            ['bitcoinTestnet', BitcoinTestnet::class],
-            ['bitcoinRegtest', BitcoinRegtest::class],
-            ['dash', Dash::class],
-            ['dashTestnet', DashTestnet::class],
-            ['dogecoin', Dogecoin::class],
-            ['dogecoinTestnet', DogecoinTestnet::class],
-            ['litecoin', Litecoin::class],
-            ['litecoinTestnet', LitecoinTestnet::class],
-            ['viacoin', Viacoin::class],
-            ['viacoinTestnet', ViacoinTestnet::class],
+            ['bitcoin', Networks\Bitcoin::class],
+            ['bitcoinTestnet', Networks\BitcoinTestnet::class],
+            ['bitcoinRegtest', Networks\BitcoinRegtest::class],
+            ['dash', Networks\Dash::class],
+            ['dashTestnet', Networks\DashTestnet::class],
+            ['dogecoin', Networks\Dogecoin::class],
+            ['dogecoinTestnet', Networks\DogecoinTestnet::class],
+            ['litecoin', Networks\Litecoin::class],
+            ['litecoinTestnet', Networks\LitecoinTestnet::class],
+            ['viacoin', Networks\Viacoin::class],
+            ['viacoinTestnet', Networks\ViacoinTestnet::class],
         ];
     }
 
     /**
-     * @param $method
-     * @param $expectedClass
+     * @param string $method
+     * @param string $expectedClass
      * @dataProvider getFactoryMethodAndClass
      */
-    public function testNetworkFactory($method, $expectedClass)
+    public function testNetworkFactory(string $method, string $expectedClass)
     {
         $this->assertInstanceOf($expectedClass, call_user_func([NetworkFactory::class, $method]));
     }
