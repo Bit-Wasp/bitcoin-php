e905d7f5f8c0911b3b5eee82a4045cb549c69d8d
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-11-23 02:41:15 +0100

    try running phpstan with extensions

--- travis/run_phpunit_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$PHPUNIT_EXT" = "true" ]; then
-  EXT_PHP='-dextension="secp256k1.so" -dextension="bitcoinconsensus.so"' make phpunit-ci;
-elif [ "$PHPUNIT" = "true" ]; then
-  make phpunit-ci;
-fi
