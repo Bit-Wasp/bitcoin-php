aa75ce745d0ea315cb7a0133fe5791ce3f924d43
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-12-04 01:43:56 +0100

    DerSignatureSerializer: rewrite to avoid Templates, and add consistency test for the class

--- src/Crypto/EcAdapter/Impl/PhpEcc/Serializer/Signature/DerSignatureSerializer.php
+++ src/Crypto/EcAdapter/Impl/PhpEcc/Serializer/Signature/DerSignatureSerializer.php
@@ -9,12 +9,12 @@ use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Adapter\EcAdapter;
 use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Signature\Signature;
 use BitWasp\Bitcoin\Crypto\EcAdapter\Serializer\Signature\DerSignatureSerializerInterface;
 use BitWasp\Bitcoin\Crypto\EcAdapter\Signature\SignatureInterface;
+use BitWasp\Bitcoin\Serializer\Types;
 use BitWasp\Buffertools\Buffer;
 use BitWasp\Buffertools\BufferInterface;
+use BitWasp\Buffertools\Buffertools;
 use BitWasp\Buffertools\Exceptions\ParserOutOfRange;
 use BitWasp\Buffertools\Parser;
-use BitWasp\Buffertools\Template;
-use BitWasp\Buffertools\TemplateFactory;
 
 class DerSignatureSerializer implements DerSignatureSerializerInterface
 {
@@ -24,11 +24,17 @@ class DerSignatureSerializer implements DerSignatureSerializerInterface
     private $ecAdapter;
 
     /**
+     * @var \BitWasp\Buffertools\Types\VarString
+     */
+    private $varstring;
+
+    /**
      * @param EcAdapter $adapter
      */
     public function __construct(EcAdapter $adapter)
     {
         $this->ecAdapter = $adapter;
+        $this->varstring = Types::varstring();
     }
 
     /**
@@ -40,63 +46,34 @@ class DerSignatureSerializer implements DerSignatureSerializerInterface
     }
 
     /**
-     * @return Template
-     */
-    private function getInnerTemplate(): Template
-    {
-        return (new TemplateFactory())
-            ->uint8()
-            ->varstring()
-            ->uint8()
-            ->varstring()
-            ->getTemplate();
-    }
-
-    /**
-     * @return Template
-     */
-    private function getOuterTemplate(): Template
-    {
-        return (new TemplateFactory())
-            ->uint8()
-            ->varstring()
-            ->getTemplate();
-    }
-
-    /**
      * @param SignatureInterface $signature
      * @return BufferInterface
      * @throws \Exception
      */
     public function serialize(SignatureInterface $signature): BufferInterface
     {
-        $math = $this->ecAdapter->getMath();
-
         // Ensure that the R and S hex's are of even length
-        $rBin = $math->intToString($signature->getR());
-        $sBin = $math->intToString($signature->getS());
+        $rBin = gmp_export($signature->getR(), 1, GMP_MSW_FIRST | GMP_BIG_ENDIAN);
+        $sBin = gmp_export($signature->getS(), 1, GMP_MSW_FIRST | GMP_BIG_ENDIAN);
 
         // Pad R and S if their highest bit is flipped, ie,
         // they are negative.
-        $rt = $rBin[0] & pack('H*', '80');
-        if (ord($rt) === 128) {
-            $rBin = pack('H*', '00') . $rBin;
+        if ((ord($rBin[0]) & 0x80) === 0x80) {
+            $rBin = "\x00$rBin";
         }
 
-        $st = $sBin[0] & pack('H*', '80');
-        if (ord($st) === 128) {
-            $sBin = pack('H*', '00') . $sBin;
+        if ((ord($sBin[0]) & 0x80) === 0x80) {
+            $sBin = "\x00$sBin";
         }
 
-        return $this->getOuterTemplate()->write([
-            0x30,
-            $this->getInnerTemplate()->write([
-                0x02,
-                new Buffer($rBin, null),
-                0x02,
-                new Buffer($sBin, null)
-            ])
-        ]);
+        $inner = sprintf("\x02%s%s\x02%s%s",
+            Buffertools::numToVarIntBin(strlen($rBin)),
+            $rBin,
+            Buffertools::numToVarIntBin(strlen($sBin)),
+            $sBin
+        );
+
+        return new Buffer(sprintf("\x30%s%s", Buffertools::numToVarIntBin(strlen($inner)), $inner));
     }
 
     /**
@@ -106,20 +83,31 @@ class DerSignatureSerializer implements DerSignatureSerializerInterface
      */
     public function fromParser(Parser $parser): SignatureInterface
     {
-        try {
-            list (, $inner) = $this->getOuterTemplate()->parse($parser);
-            list (, $r, , $s) = $this->getInnerTemplate()->parse(new Parser($inner));
-            /** @var Buffer $r */
-            /** @var Buffer $s */
+        $prefix = $parser->readBytes(1);
+        if ($prefix->getBinary() != "\x30") {
+            throw new \RuntimeException("invalid signature");
+        }
+        $inner = $this->varstring->read($parser);
 
-            return new Signature(
-                $this->ecAdapter,
-                $r->getGmp(),
-                $s->getGmp()
-            );
+        try {
+            $pinner = new Parser($inner);
+
+            $rPref = $pinner->readBytes(1);
+            if ($rPref->getBinary() != "\x02") {
+                throw new \RuntimeException("invalid signature");
+            }
+            $r = $this->varstring->read($pinner);
+
+            $sPref = $pinner->readBytes(1);
+            if ($sPref->getBinary() != "\x02") {
+                throw new \RuntimeException("invalid signature");
+            }
+            $s = $this->varstring->read($pinner);
         } catch (ParserOutOfRange $e) {
             throw new ParserOutOfRange('Failed to extract full signature from parser');
         }
+
+        return new Signature($this->ecAdapter, $r->getGmp(), $s->getGmp());
     }
 
     /**
--- tests/Serializer/Signature/DerSignatureSerializerTest.php
+++ tests/Serializer/Signature/DerSignatureSerializerTest.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace BitWasp\Bitcoin\Tests\Serializer\Signature;
 
 use BitWasp\Bitcoin\Crypto\EcAdapter\Adapter\EcAdapterInterface;
+use BitWasp\Bitcoin\Crypto\EcAdapter\EcAdapterFactory;
 use BitWasp\Bitcoin\Crypto\EcAdapter\EcSerializer;
+use BitWasp\Bitcoin\Crypto\EcAdapter\Impl\PhpEcc\Signature\Signature as PhpeccSignature;
 use BitWasp\Bitcoin\Crypto\EcAdapter\Serializer\Signature\DerSignatureSerializerInterface;
+use BitWasp\Bitcoin\Math\Math;
 use BitWasp\Bitcoin\Tests\AbstractTestCase;
 use BitWasp\Buffertools\Buffer;
+use Mdanter\Ecc\EccFactory;
 
 class DerSignatureSerializerTest extends AbstractTestCase
 {
@@ -22,5 +26,18 @@ class DerSignatureSerializerTest extends AbstractTestCase
         /** @var DerSignatureSerializerInterface $serializer */
         $serializer = EcSerializer::getSerializer(DerSignatureSerializerInterface::class, true, $adapter);
         $serializer->parse(new Buffer());
+    }
+
+    public function testPhpeccIsConsistent() {
+        $r = 1;
+        $s = 1;
+        $adapter = EcAdapterFactory::getPhpEcc(new Math(), EccFactory::getSecgCurves()->generator256k1());
+        $signature = new PhpeccSignature($adapter, gmp_init($r), gmp_init($s));
+        /** @var DerSignatureSerializerInterface $serializer */
+        $serializer = EcSerializer::getSerializer(DerSignatureSerializerInterface::class, true, $adapter);
+        $buffer = $serializer->serialize($signature);
+        $parsed = $serializer->parse($buffer);
+        $this->assertEquals($signature->getR(), $parsed->getR());
+        $this->assertEquals($signature->getS(), $parsed->getS());
     }
 }
