6f7d4f27a1fbc3e37ae19cb0d9b16f18e75e2c4b
Thomas Kerin <me@thomaskerin.io>
2021-04-24 21:05:38 +0100

    AbstractCollectionMutator: refactor out SqlFixedArray which has had a BC break (update InputCollectionMutator + OutputCollectionMutator)

--- src/Transaction/Mutator/InputCollectionMutator.php
+++ src/Transaction/Mutator/InputCollectionMutator.php
@@ -8,7 +8,6 @@ use BitWasp\Bitcoin\Transaction\TransactionInputInterface;
 
 class InputCollectionMutator extends AbstractCollectionMutator
 {
-
     /**
      * @param TransactionInputInterface[] $inputs
      */
@@ -20,15 +19,12 @@ class InputCollectionMutator extends AbstractCollectionMutator
             $set[$i] = new InputMutator($input);
         }
 
-        $this->set = \SplFixedArray::fromArray($set, false);
+        $this->set = $set;
     }
 
-    /**
-     * @return InputMutator
-     */
     public function current(): InputMutator
     {
-        return $this->set->current();
+        return parent::current();
     }
 
     /**
@@ -37,11 +33,7 @@ class InputCollectionMutator extends AbstractCollectionMutator
      */
     public function offsetGet($offset): InputMutator
     {
-        if (!$this->set->offsetExists($offset)) {
-            throw new \OutOfRangeException('Input does not exist');
-        }
-
-        return $this->set->offsetGet($offset);
+        return parent::offsetGet($offset);
     }
 
     /**
@@ -58,39 +50,30 @@ class InputCollectionMutator extends AbstractCollectionMutator
     }
 
     /**
-     * @param int $start
-     * @param int $length
-     * @return $this
+     * Return an array containing values beginning at index $start and ending
+     * with index $start + $length. An exception is thrown if start or $length
+     * is out of bounds
      */
-    public function slice(int $start, int $length)
+    public function slice(int $start, int $length): InputCollectionMutator
     {
-        $end = $this->set->getSize();
+        $end = $this->count();
         if ($start > $end || $length > $end) {
             throw new \RuntimeException('Invalid start or length');
         }
 
-        $this->set = \SplFixedArray::fromArray(array_slice($this->set->toArray(), $start, $length), false);
+        $this->set = array_slice($this->set, $start, $length);
         return $this;
     }
 
-    /**
-     * @return $this
-     */
-    public function null()
+    public function null(): InputCollectionMutator
     {
         $this->slice(0, 0);
         return $this;
     }
 
-    /**
-     * @param TransactionInputInterface $input
-     * @return $this
-     */
-    public function add(TransactionInputInterface $input)
+    public function add(TransactionInputInterface $input): InputCollectionMutator
     {
-        $size = $this->set->getSize();
-        $this->set->setSize($size + 1);
-
+        $size = $this->count();
         $this->set[$size] = new InputMutator($input);
         return $this;
     }
@@ -100,9 +83,12 @@ class InputCollectionMutator extends AbstractCollectionMutator
      * @param TransactionInputInterface $input
      * @return $this
      */
-    public function set(int $i, TransactionInputInterface $input)
+    public function set(int $i, TransactionInputInterface $input): InputCollectionMutator
     {
-        $this->set[$i] = new InputMutator($input);
+        if ($i > count($this->set)) {
+            throw new \InvalidArgumentException();
+        }
+        $this->set[$i] = $input;
         return $this;
     }
 }
