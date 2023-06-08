eacb3d0f084684be4e347d1ca4fe93b093fdc20e
Maximilian Schmidt <schmidtmaximilian@online.de>
2023-06-08 22:22:01 +0200

    SplFixedArray not longer supported with rewind and valid in PHP8.x

--- src/Transaction/Mutator/AbstractCollectionMutator.php
+++ src/Transaction/Mutator/AbstractCollectionMutator.php
@@ -3,117 +3,87 @@
 declare(strict_types=1);
 
 namespace BitWasp\Bitcoin\Transaction\Mutator;
+use ArrayAccess;
+use Countable;
+use Iterator;
+use SplFixedArray;
 
-abstract class AbstractCollectionMutator implements \Iterator, \ArrayAccess, \Countable
+abstract class AbstractCollectionMutator implements Iterator, ArrayAccess, Countable
 {
-    /**
-     * @var \SplFixedArray
-     */
-    protected $set;
+    private $array;
+    private $position;
 
-    /**
-     * @return array
-     */
+    public function __construct(int $size)
+    {
+        $this->array = new SplFixedArray($size);
+        $this->position = 0;
+    }
+
     public function all(): array
     {
         return $this->set->toArray();
     }
 
-    /**
-     * @return bool
-     */
     public function isNull(): bool
     {
         return count($this->set) === 0;
     }
 
-    /**
-     * @return int
-     */
-    public function count(): int
-    {
-        return $this->set->count();
-    }
-
-    /**
-     *
-     */
     public function rewind()
     {
-        $this->set->rewind();
+        $this->position = 0;
     }
 
-    /**
-     * @return mixed
-     */
     public function current()
     {
-        return $this->set->current();
+        return $this->array[$this->position];
     }
 
-    /**
-     * @return int
-     */
     public function key()
     {
-        return $this->set->key();
+        return $this->position;
     }
 
-    /**
-     *
-     */
     public function next()
     {
-        $this->set->next();
+        $this->position++;
     }
 
-    /**
-     * @return bool
-     */
     public function valid()
     {
-        return $this->set->valid();
+        return isset($this->array[$this->position]);
     }
 
-    /**
-     * @param int $offset
-     * @return bool
-     */
     public function offsetExists($offset)
     {
-        return $this->set->offsetExists($offset);
+        return isset($this->array[$offset]);
     }
 
-    /**
-     * @param int $offset
-     */
+    public function offsetGet($offset)
+    {
+        return $this->array[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if (!$this->set->offsetExists($offset)) {
+            throw new \OutOfRangeException('Nothing found at this offset');
+        }
+
+        $this->array[$offset] = $value;
+    }
+
     public function offsetUnset($offset)
     {
         if (!$this->offsetExists($offset)) {
             throw new \InvalidArgumentException('Offset does not exist');
         }
 
-        $this->set->offsetUnset($offset);
+        unset($this->array[$offset]);
     }
 
-    /**
-     * @param int $offset
-     * @return mixed
-     */
-    public function offsetGet($offset)
+    public function count()
     {
-        if (!$this->set->offsetExists($offset)) {
-            throw new \OutOfRangeException('Nothing found at this offset');
-        }
-        return $this->set->offsetGet($offset);
-    }
-
-    /**
-     * @param int $offset
-     * @param mixed $value
-     */
-    public function offsetSet($offset, $value)
-    {
-        $this->set->offsetSet($offset, $value);
+        return $this->array->count();
     }
 }
